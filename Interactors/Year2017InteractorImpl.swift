35f67ed2baa37ab7fd306ed9af7f556b26a73c9c
Javi <javi_castillo18@hotmail.com>
2022-02-21 11:19:19 +0100

    Create solution for question 2 in day 3 for year 2017

--- Interactors/Year2017InteractorImpl.swift
+++ Interactors/Year2017InteractorImpl.swift
@@ -80,6 +80,32 @@ extension Year2017InteractorImpl: YearInteractor {
     
     @objc
     func day3question2() -> String {
+        let target = 277678
+        let size = 101
+        var matrix: [[Int]] = [[Int]](repeating: [Int](repeating: 0, count: size), count: size)
+        let center = size/2
+        matrix[center][center] = 1
+        matrix[center][center+1] = 1
+        var row = center - 1
+        var col = center + 1
+        var direction = 0
+        var step = 3
+        while row > 0 && row < size-1 && col > 0 && col < size-1 {
+            let numSteps = direction == 0 ? step-2 : direction == 3 ? step : step - 1
+            for _ in 0..<numSteps {
+                let val = sumNeighbors(matrix: matrix, row: row, col: col)
+                if val > target {
+                    return String(val)
+                }
+                matrix[row][col] = val
+                row += direction == 0 ? -1 : direction == 2 ? 1 : 0
+                col += direction == 1 ? -1 : direction == 3 ? 1 : 0
+            }
+            row += direction == 0 || direction == 1 ? 1 : -1
+            col += direction == 1 || direction == 2 ? 1 : -1
+            direction = (direction+1)%4
+            if direction == 0 { step += 2 }
+        }
         return ""
     }
     
@@ -92,6 +118,17 @@ extension Year2017InteractorImpl: YearInteractor {
                            abs(input-firstCenter-2*diff),
                            abs(input-firstCenter-3*diff)].min()!
         return row - 1 + minDistance
+    }
+    
+    private func sumNeighbors(matrix: [[Int]], row: Int, col: Int) -> Int {
+        matrix[row][col-1]
+        + matrix[row][col+1]
+        + matrix[row-1][col]
+        + matrix[row-1][col-1]
+        + matrix[row-1][col+1]
+        + matrix[row+1][col]
+        + matrix[row+1][col-1]
+        + matrix[row+1][col+1]
     }
     
 }
