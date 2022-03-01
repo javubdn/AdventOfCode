100423cfc747552463fa66fa1ac9c2361601ad1e
Javi <javi_castillo18@hotmail.com>
2022-03-01 08:51:55 +0100

    Create solution for question 1 in day 14 for year 2017

--- Interactors/Year2017InteractorImpl.swift
+++ Interactors/Year2017InteractorImpl.swift
@@ -631,4 +631,22 @@ extension Year2017InteractorImpl: YearInteractor {
         return String(result)
     }
     
+    @objc
+    func day14question1() -> String {
+        let input = "hfdlxzhv"
+        let number1s = ["0": 0, "1": 1, "2": 1, "3": 2, "4": 1, "5": 2, "6": 2, "7": 3, "8": 1, "9": 2, "a": 2, "b": 3, "c": 2, "d": 3, "e": 3, "f": 4]
+        var result = 0
+        for i in 0...127 {
+            let value = "\(input)-\(i)"
+            let k = knot(value)
+            result += k.map { number1s[String($0)]! }.reduce(0, +)
+        }
+        return String(result)
+    }
+    
+    @objc
+    func day14question2() -> String {
+        return ""
+    }
+    
 }
