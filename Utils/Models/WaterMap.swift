f237127f126882472490ca6903c1f0053fe111b9
Javi <javi_castillo18@hotmail.com>
2022-04-12 15:19:34 +0200

    Change queue from array to Set

--- Utils/Models/WaterMap.swift
+++ Utils/Models/WaterMap.swift
@@ -25,6 +25,12 @@ private class Position: Hashable {
     
 }
 
+private struct ActionQueue: Hashable {
+    let action: Action
+    let x: Int
+    let y: Int
+}
+
 private enum Action {
     case fall
     case scan
@@ -32,14 +38,14 @@ private enum Action {
 
 class WaterMap {
     
-    fileprivate var clay: Set<Position> = Set()
-    fileprivate var still: Set<Position> = Set()
-    fileprivate var flowing: Set<Position> = Set()
+    fileprivate var clay: Set<Position> = []
+    fileprivate var still: Set<Position> = []
+    fileprivate var flowing: Set<Position> = []
     private var minX: Int
     private var minY: Int
     private var maxX: Int
     private var maxY: Int
-    private var queue: [(Action, (x: Int, y: Int))] = []
+    private var queue: Set<ActionQueue> = []
     
     init(_ lines: [String]) {
         
@@ -66,19 +72,22 @@ class WaterMap {
     }
     
     func run(_ x: Int, _ y: Int) {
-        queue.append((.fall, (x: x, y: y)))
+        queue.insert(ActionQueue(action: .fall, x: x, y: y))
         while !queue.isEmpty {
             let instruction = queue.removeFirst()
-            switch instruction.0 {
-            case .fall: fall(instruction.1.x, instruction.1.y)
-            case .scan: scan(instruction.1.x, instruction.1.y)
+            switch instruction.action {
+            case .fall: fall(instruction.x, instruction.y)
+            case .scan: scan(instruction.x, instruction.y)
             }
         }
-        print("Done")
     }
     
     func countAll() -> Int {
         still.union(flowing).filter { $0.y >= minY && $0.y <= maxY }.count
+    }
+    
+    func countStill() -> Int {
+        still.filter { $0.y >= minY && $0.y <= maxY }.count
     }
     
     func printS() {
@@ -94,7 +103,6 @@ class WaterMap {
         for fl in flowing {
             waterMap[fl.y][fl.x-minX+1] = "|"
         }
-        
         let valu = waterMap.map { $0.joined() }
         valu.forEach { print($0) }
     }
@@ -113,9 +121,9 @@ class WaterMap {
             flowing.insert(Position(x: x, y: y))
             y += 1
         }
-        if y <= maxY {
+        if y <= maxY && !flowing.contains(Position(x: x, y: y)) {
             flowing.insert(Position(x: x, y: y))
-            queue.append((.scan, (x: x, y: y)))
+            queue.insert(ActionQueue(action: .scan, x: x, y: y))
         }
     }
     
@@ -134,16 +142,16 @@ class WaterMap {
             for i in x0..<x1+1 {
                 still.insert(Position(x: i, y: y))
             }
-            queue.append((.scan, (x: x, y: y - 1)))
+            queue.insert(ActionQueue(action: .scan, x: x, y: y - 1))
         } else {
             for i in x0..<x1+1 {
                 flowing.insert(Position(x: i, y: y))
             }
             if !stop0 {
-                queue.append((.fall, (x: x0, y: y)))
+                queue.insert(ActionQueue(action: .fall, x: x0, y: y))
             }
             if !stop1 {
-                queue.append((.fall, (x: x1, y: y)))
+                queue.insert(ActionQueue(action: .fall, x: x1, y: y))
             }
         }
     }
