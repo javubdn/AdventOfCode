dc8042cc9c9fb00b632bbccbc0cf43c56ccef8a2
Javi <javi_castillo18@hotmail.com>
2022-05-11 12:55:33 +0200

    Create solution for question 1 in day 3 for year 2019

--- Interactors/Year2019InteractorImpl.swift
+++ Interactors/Year2019InteractorImpl.swift
@@ -12,4 +12,11 @@ class Year2019InteractorImpl: NSObject {
 
 extension Year2019InteractorImpl: YearInteractor {
     
+    @objc
+    func day1question1() -> String {
+        let input = readCSV("InputYear2019Day1").components(separatedBy: .newlines).map { Int($0)! }
+        let result = input.map { $0/3 - 2 }.reduce(0, +)
+        return String(result)
+    }
+    
 }
