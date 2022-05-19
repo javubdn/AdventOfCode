7d3f8625c55275a5353486f25bcbd2f94aa5f4ea
Javi <javi_castillo18@hotmail.com>
2022-05-19 08:33:02 +0200

    Changes in Intcode to use new parameters with mode 2

--- Utils/Models/Intcode.swift
+++ Utils/Models/Intcode.swift
@@ -19,6 +19,7 @@ class Intcode {
     var input: [Int] = []
     var output: [Int] = []
     var completed = false
+    var relativeBase = 0
     
     init(instructions: [Int]) {
         self.instructions = instructions
@@ -32,57 +33,71 @@ class Intcode {
         while ip < instructions.count {
             let instruction = instructions[ip]
             let opcode = instruction % 100
-            let firstParamReference = instruction/100 % 10 == 0
-            let secondParamReference = instruction/1000 % 10 == 0
             switch opcode {
             case 1:
-                instructions[instructions[ip+3]] = (firstParamReference ? instructions[instructions[ip+1]] : instructions[ip+1])
-                + (secondParamReference ? instructions[instructions[ip+2]] : instructions[ip+2])
+                instructions[getParam(3, read: false)] = getParam(1, read: true) + getParam(2, read: true)
                 ip += 4
             case 2:
-                let firstParam = firstParamReference ? instructions[instructions[ip+1]] : instructions[ip+1]
-                let secondParam = secondParamReference ? instructions[instructions[ip+2]] : instructions[ip+2]
-                instructions[instructions[ip+3]] = firstParam * secondParam
+                instructions[getParam(3, read: false)] = getParam(1, read: true) * getParam(2, read: true)
                 ip += 4
             case 3:
                 if !input.isEmpty {
-                    instructions[instructions[ip+1]] = input.removeFirst()
+                    instructions[getParam(1, read: false)] = input.removeFirst()
                 }
                 ip += 2
             case 4:
-                output.append(instructions[instructions[ip+1]])
+                output.append(getParam(1, read: true))
                 ip += 2
                 if mode == .partial {
                     return
                 }
             case 5:
-                if (firstParamReference ? instructions[instructions[ip+1]] : instructions[ip+1]) != 0 {
-                    ip = secondParamReference ? instructions[instructions[ip+2]] : instructions[ip+2]
+                if getParam(1, read: true) != 0 {
+                    ip = getParam(2, read: true)
                 } else {
                     ip += 3
                 }
             case 6:
-                if (firstParamReference ? instructions[instructions[ip+1]] : instructions[ip+1]) == 0 {
-                    ip = secondParamReference ? instructions[instructions[ip+2]] : instructions[ip+2]
+                if getParam(1, read: true) == 0 {
+                    ip = getParam(2, read: true)
                 } else {
                     ip += 3
                 }
             case 7:
-                let firstValue = firstParamReference ? instructions[instructions[ip+1]] : instructions[ip+1]
-                let secondValue = secondParamReference ? instructions[instructions[ip+2]] : instructions[ip+2]
-                instructions[instructions[ip+3]] = firstValue < secondValue ? 1 : 0
+                instructions[getParam(3, read: false)] = getParam(1, read: true) < getParam(2, read: true) ? 1 : 0
                 ip += 4
             case 8:
-                let firstValue = firstParamReference ? instructions[instructions[ip+1]] : instructions[ip+1]
-                let secondValue = secondParamReference ? instructions[instructions[ip+2]] : instructions[ip+2]
-                instructions[instructions[ip+3]] = firstValue == secondValue ? 1 : 0
+                instructions[getParam(3, read: false)] = getParam(1, read: true) == getParam(2, read: true) ? 1 : 0
                 ip += 4
+            case 9:
+                relativeBase += getParam(1, read: true)
+                ip += 2
             case 99:
                 completed = true
                 return
             default: break
             }
         }
+    }
+    
+    private func getParam(_ num: Int, read: Bool) -> Int {
+        let instruction = instructions[ip]
+        let mode = num == 1 ? instruction/100 % 10 : num == 2 ? instruction/1000 % 10 : instruction/10000 % 10
+        return read ? instructions[getInstructionAddress(num, mode)] : getInstructionAddress(num, mode)
+    }
+    
+    private func getInstructionAddress(_ param: Int, _ paramMode: Int) -> Int {
+        var position = 0
+        switch paramMode {
+        case 0: position = instructions[ip+param]
+        case 1: position = ip+param
+        case 2: position = relativeBase+instructions[ip+param]
+        default: break
+        }
+        if position >= instructions.count {
+            instructions += [Int](repeating: 0, count: position-instructions.count+1)
+        }
+        return position
     }
     
 }
