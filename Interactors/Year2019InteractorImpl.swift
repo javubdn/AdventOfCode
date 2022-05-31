d4e3f1c904a25c23cbe959a05d096da3c92d2f82
Javi <javi_castillo18@hotmail.com>
2022-05-31 11:42:13 +0200

    Unify methods in solutions for question in day 15 for year 2019

--- Interactors/Year2019InteractorImpl.swift
+++ Interactors/Year2019InteractorImpl.swift
@@ -717,39 +717,32 @@ extension Year2019InteractorImpl: YearInteractor {
     @objc
     func day15question1() -> String {
         let input = readCSV("InputYear2019Day15").components(separatedBy: ",").map { Int($0)! }
-        var movements: [([Int], (Int, Int))] = [([1], (21, 20)),([2], (21, 22)),([3], (20, 21)),([4], (22, 21))]
-        var visited = [[Int]](repeating: [Int](repeating: -1, count: 41), count: 41)
-        visited[21][21] = 1
-        while !movements.isEmpty {
-            let movement = movements.removeFirst()
-            let intcode = Intcode(instructions: input)
-            intcode.addInput(movement.0)
-            intcode.execute()
-            let output = intcode.readOutput()
-            let nextValue = output.last!
-            if nextValue == 0 {
-                visited[movement.1.1][movement.1.0] = 2
-            } else if nextValue == 1 {
-                visited[movement.1.1][movement.1.0] = 1
-                for nextMovement in 1...4 {
-                    let nextX = movement.1.0 + (nextMovement == 3 ? -1 : nextMovement == 4 ? 1 : 0)
-                    let nextY = movement.1.1 + (nextMovement == 1 ? -1 : nextMovement == 2 ? 1 : 0)
-                    if visited[nextY][nextX] == -1 {
-                        movements.append((movement.0 + [nextMovement], (nextX, nextY)))
-                    }
-                }
-            } else if nextValue == 2 {
-//                let printation = visited.map { $0.map { $0 == -1 ? "?" : $0 == 1 ? "." : "#" }.joined() }
-//                printation.forEach { print($0) }
-                return "\(movement.0.count)"
-            }
-        }
-        return ""
+        let (result, _, _) = calculateLeak(input, fullMap: false)
+        return "\(result)"
     }
     
     @objc
     func day15question2() -> String {
         let input = readCSV("InputYear2019Day15").components(separatedBy: ",").map { Int($0)! }
+        var (_, positionRepair, visited) = calculateLeak(input, fullMap: true)
+        var positions = [(positionRepair, 0)]
+        var time = 0
+        while !positions.isEmpty {
+            let position = positions.removeFirst()
+            visited[position.0.1][position.0.0] = 3
+            for nextMovement in 1...4 {
+                let nextX = position.0.0 + (nextMovement == 3 ? -1 : nextMovement == 4 ? 1 : 0)
+                let nextY = position.0.1 + (nextMovement == 1 ? -1 : nextMovement == 2 ? 1 : 0)
+                if visited[nextY][nextX] == 1 {
+                    positions.append(((nextX, nextY), position.1 + 1))
+                }
+            }
+            time = max(time, position.1)
+        }
+        return "\(time)"
+    }
+    
+    private func calculateLeak(_ input: [Int], fullMap: Bool) -> (Int, (Int, Int), [[Int]]) {
         var movements: [([Int], (Int, Int))] = [([1], (21, 20)),([2], (21, 22)),([3], (20, 21)),([4], (22, 21))]
         var visited = [[Int]](repeating: [Int](repeating: -1, count: 41), count: 41)
         visited[21][21] = 1
@@ -775,22 +768,10 @@ extension Year2019InteractorImpl: YearInteractor {
             } else if nextValue == 2 {
                 visited[movement.1.1][movement.1.0] = 0
                 positionRepair = movement.1
+                if !fullMap { return (movement.0.count, positionRepair, visited) }
             }
         }
-        var positions = [(positionRepair, 0)]
-        var time = 0
-        while !positions.isEmpty {
-            let position = positions.removeFirst()
-            visited[position.0.1][position.0.0] = 3
-            for nextMovement in 1...4 {
-                let nextX = position.0.0 + (nextMovement == 3 ? -1 : nextMovement == 4 ? 1 : 0)
-                let nextY = position.0.1 + (nextMovement == 1 ? -1 : nextMovement == 2 ? 1 : 0)
-                if visited[nextY][nextX] == 1 {
-                    positions.append(((nextX, nextY), position.1 + 1))
-                }
-            }
-            time = max(time, position.1)
-        }
-        return "\(time)"
+        return (0, positionRepair, visited)
     }
+    
 }
