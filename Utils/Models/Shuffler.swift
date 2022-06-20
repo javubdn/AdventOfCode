fa5528ba81b444cd6c7476b61f752c54fe4cb9d1
Javi <javi_castillo18@hotmail.com>
2022-06-20 11:47:36 +0200

    Create method to calculate modular arithmetics

--- Utils/Models/Shuffler.swift
+++ Utils/Models/Shuffler.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import BigInt
 
 class Shuffler {
     
@@ -64,6 +65,35 @@ class Shuffler {
             }
         }
         return cards
+    }
+    
+    func modularArithmeticVersion(_ numberCards: Int, _ numberShuffles: Int, _ find: Int) -> Int {
+        let numberCardsBI = BigInt(numberCards)
+        let findBI = BigInt(find)
+        var memory = [BigInt(1), BigInt(0)]
+        instructions.reversed().forEach { instruction in
+            switch instruction.type {
+            case .cut:
+                memory[1] = memory[1] + BigInt(instruction.value)
+            case .increment:
+                let exponent = Int(numberCardsBI - BigInt(2))
+                let potencia = BigInt(instruction.value).power(exponent)
+                let val = potencia.modulus(numberCardsBI)
+                memory[0] *= val
+                memory[1] *= val
+            case .newStack:
+                memory[0] = -memory[0]
+                memory[1] = -memory[1] - 1
+            }
+            memory[0] = memory[0].modulus(numberCardsBI)
+            memory[1] = memory[1].modulus(numberCardsBI)
+        }
+        let power = memory[0].power(numberShuffles).modulus(numberCardsBI)
+        let item1 = power * findBI
+        let item2 = memory[1] * (power + numberCardsBI)
+        let item3 = memory[0].power(numberCards - 2).modulus(numberCardsBI)
+        return Int((item1 + item2 * item3).modulus(numberCardsBI))
+
     }
     
 }
