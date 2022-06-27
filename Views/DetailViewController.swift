acaf0f41f545e2b7fc39b1860603d5942af7e958
Javi <javi_castillo18@hotmail.com>
2022-06-27 10:51:13 +0200

    Create DetailViewController with the associated xib

---
+++ Views/DetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewController.swift
+//  AdventOfCode
+//
+//  Created by Javier Castillo on 23/6/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
