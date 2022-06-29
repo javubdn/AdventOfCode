298dabe7059b5cd9df9d4dacd6a51bcede24e0e6
Javi <javi_castillo18@hotmail.com>
2022-06-29 12:31:42 +0200

    Update DetailViewController to use year and day

--- Views/DetailViewController.swift
+++ Views/DetailViewController.swift
@@ -9,7 +9,14 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    init() {
+    private let year: Year
+    private let day: Int
+    
+    @IBOutlet weak var mainStackView: UIStackView!
+    
+    init(_ year: Year, _ day: Int) {
+        self.year = year
+        self.day = day
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -17,8 +24,12 @@ class DetailViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if day == 24 && year == .nineteen {
+            let detailView = Detail2019_25View(mainStackView)
+            detailView.initializeComputer()
+        }
     }
 
 }
