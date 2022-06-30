34dcd8d22e9cbac15b3be1a25933fcff07c8e957
Javi <javi_castillo18@hotmail.com>
2022-06-30 09:10:54 +0200

    Create interactor for year 2020

---
+++ Interactors/Year2020InteractorImpl.swift
@@ -0,0 +1,15 @@
+//
+//  Year2020InteractorImpl.swift
+//  AdventOfCode
+//
+//  Created by Javier Castillo on 28/6/22.
+//
+
+import Foundation
+
+class Year2020InteractorImpl: NSObject {
+}
+
+extension Year2020InteractorImpl: YearInteractor {
+    
+}
