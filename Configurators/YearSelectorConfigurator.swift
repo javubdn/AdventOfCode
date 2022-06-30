e72699920c5a8dc7143da59e03a70f4ea13b9464
Javi <javi_castillo18@hotmail.com>
2022-06-30 09:06:14 +0200

    Fix YearSelectorConfigurator to return the view instead the navigation

--- Configurators/YearSelectorConfigurator.swift
+++ Configurators/YearSelectorConfigurator.swift
@@ -9,13 +9,12 @@ import UIKit
 
 class YearSelectorConfigurator {
     
-    static func configure(_ window: UIWindow?) {
+    static func configure(navigationController: UINavigationController) -> UIViewController {
         let router = YearSelectorRouterImpl()
         let presenter = YearSelectorPresenterImpl(router: router)
         let viewController = YearSelectorViewController(presenter: presenter)
-        let navigationController = UINavigationController(rootViewController: viewController)
         router.setNavigationController(navigationController)
-        window?.rootViewController = navigationController
+        return viewController
     }
     
 }
