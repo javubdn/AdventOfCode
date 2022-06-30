1f58d377a388574908a80d2d6dbfccdd0aff7a02
Javi <javi_castillo18@hotmail.com>
2022-06-30 09:07:27 +0200

    Add year selected to MainConfigurator

--- Configurators/MainConfigurator.swift
+++ Configurators/MainConfigurator.swift
@@ -9,13 +9,12 @@ import UIKit
 
 class MainConfigurator {
     
-    static func configure(navigationController: UINavigationController) -> UIViewController {
+    static func configure(navigationController: UINavigationController, year: Year) -> UIViewController {
         let interactor = MainInteractorImpl()
         let router = MainRouterImpl(navigationController: navigationController)
         let presenter = MainPresenterImpl(interactor: interactor, router: router)
-        let viewController = MainViewController(presenter: presenter, currentYear: .nineteen)
-        navigationController.pushViewController(viewController, animated: false)
-        return navigationController
+        let viewController = MainViewController(presenter: presenter, currentYear: year)
+        return viewController
     }
     
 }
