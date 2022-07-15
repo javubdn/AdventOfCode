4a739cb35b538960b59d51a720bf2e5e0aa7f7b1
Javi <javi_castillo18@hotmail.com>
2022-07-15 14:10:38 +0200

    Create method to get the MathNodes for advanced maths

--- Utils/Models/MathNode.swift
+++ Utils/Models/MathNode.swift
@@ -38,6 +38,61 @@ class MathNode {
         return MathOperation(operation: operand, firstOperand: operandLeft, secondOperand: operandRight)
     }
     
+    static func createAdvance(from entry: String) -> MathNode {
+        guard entry.count > 1 else {
+            return MathValue(value: Int(String(entry[0]))!)
+        }
+        let input = entry.replacingOccurrences(of: " ", with: "")
+        var operands: [String] = []
+        var operators: [MathOp] = []
+        var index = 0
+        
+        while index < input.count {
+            if input[index] == "(" {
+                var parentesisCount = 1
+                var index2 = index+1
+                while parentesisCount > 0 {
+                    if input[index2] == ")" {
+                        parentesisCount -= 1
+                    } else if input[index2] == "(" {
+                        parentesisCount += 1
+                    }
+                    index2 += 1
+                }
+                operands.append(String(input[index+1..<index2-1]))
+                index = index2
+                continue
+            } else if input[index] == "+" {
+                operators.append(.sum)
+            } else if input[index] == "*" {
+                operators.append(.mul)
+            } else {
+                operands.append(String(input[index]))
+            }
+            index += 1
+        }
+        var operandNodes = operands.map { MathNode.createAdvance(from: $0) }
+        
+        if operandNodes.count == 1 { return operandNodes.first! }
+        
+        var indexOperator = 0
+        while indexOperator < operators.count {
+            if operators[indexOperator] == .sum {
+                let operation = MathOperation(operation: .sum, firstOperand: operandNodes[indexOperator], secondOperand: operandNodes[indexOperator+1])
+                operandNodes[indexOperator] = operation
+                operandNodes.remove(at: indexOperator+1)
+                operators.remove(at: indexOperator)
+            } else {
+                indexOperator += 1
+            }
+        }
+        var finalOperation = operandNodes.removeFirst()
+        while !operandNodes.isEmpty {
+            finalOperation = MathOperation(operation: .mul, firstOperand: finalOperation, secondOperand: operandNodes.removeFirst())
+        }
+        return finalOperation
+    }
+    
     func calculate() -> Int {
         0
     }
