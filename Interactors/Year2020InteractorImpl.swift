cea78f66a0626d5dceb556a7d9f013441a3fcee7
Javi <javi_castillo18@hotmail.com>
2022-07-26 09:30:50 +0200

    Create solution for question 1 in day 19 for year 2020

--- Interactors/Year2020InteractorImpl.swift
+++ Interactors/Year2020InteractorImpl.swift
@@ -889,6 +889,16 @@ extension Year2020InteractorImpl: YearInteractor {
         return "\(result)"
     }
     
+    @objc
+    func day19question1() -> String {
+        let input = readCSV("InputYear2020Day19").components(separatedBy: "\n\n")
+        let rules = input[0].components(separatedBy: .newlines).map { getMessageRule($0) }
+        let messages = input[1].components(separatedBy: .newlines)
+        let combinations = getAllCombinations(0, rules: rules)
+        let result = messages.filter { combinations.contains($0) }.count
+        return "\(result)"
+    }
+    
     enum MessageRuleSon {
         case final(value: String)
         case sons(items: [[Int]])
@@ -908,6 +918,25 @@ extension Year2020InteractorImpl: YearInteractor {
         }
         let sons = main[1].components(separatedBy: " | ").map { $0.components(separatedBy: .whitespaces).map { Int($0)! } }
         return MessageRule(id: id, sons: .sons(items: sons))
+    }
+    
+    private func getAllCombinations(_ idRule: Int, rules: [MessageRule] ) -> [String] {
+        let rule = rules.first { $0.id == idRule }
+        guard let rule = rule else { return [] }
+        switch rule.sons {
+        case .final(let value):
+            return [value]
+        case .sons(let items):
+            var combinations: [String] = []
+            for item in items {
+                if item.count == 1 {
+                    combinations.append(contentsOf: getAllCombinations(item[0], rules: rules))
+                } else {
+                    combinations.append(contentsOf: Utils.cartesianProduct(lhs: getAllCombinations(item[0], rules: rules), rhs: getAllCombinations(item[1], rules: rules)).map { $0.0+$0.1 })
+                }
+            }
+            return combinations
+        }
     }
     
 }
