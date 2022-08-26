7d3750bf23a627a4a5027d910b92fccc8fe905d1
Javi <javi_castillo18@hotmail.com>
2022-08-26 12:35:57 +0200

    Fix Cup classes to make it works

--- Utils/Models/Cup.swift
+++ Utils/Models/Cup.swift
@@ -19,7 +19,7 @@ class Cup {
     func nextAsList(_ n: Int) -> [Cup] {
         var nextCup = next!
         var items = [nextCup]
-        for _ in 1...n {
+        for _ in 1..<n {
             nextCup = nextCup.next
             items.append(nextCup)
         }
@@ -48,12 +48,13 @@ class Cups {
     }
     
     convenience init(from input: String) {
-        let cups = input.map { Cup(value: Int(String($0))!) }
+        var cups = input.map { Cup(value: Int(String($0))!) }
         let currentCup = cups[0]
         for index in 1..<cups.count {
             cups[index-1].next = cups[index]
         }
         cups[cups.count-1].next = currentCup
+        cups = cups.sorted { $0.value < $1.value }
         self.init(cups, currentCup)
     }
     
@@ -61,7 +62,7 @@ class Cups {
         for _ in 1...numRounds {
             playRound()
         }
-        return cups[1]
+        return cups[0]
     }
     
     func playRound() {
@@ -74,9 +75,9 @@ class Cups {
     private func calculateDestination(_ exempt: Set<Int>) -> Cup {
         var dest = currentCup.value - 1
         while exempt.contains(dest) || dest == 0 {
-            dest = dest == 0 ? cups.count - 1 : dest - 1
+            dest = dest == 0 ? cups.count : dest - 1
         }
-        return cups[dest]
+        return cups[dest-1]
     }
     
     private func moveCups(_ cupsToInsert: [Cup], _ destination: Cup) {
