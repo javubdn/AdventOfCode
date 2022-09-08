c1020a8c0ab9b657761abc1a3f47337187db17c3
Javi <javi_castillo18@hotmail.com>
2022-09-08 09:28:46 +0200

    Create SnailFishNumber class

---
+++ Utils/Models/SnailFishNumber.swift
@@ -0,0 +1,16 @@
+//
+//  SnailFishNumber.swift
+//  AdventOfCode
+//
+//  Created by Javier Castillo on 6/9/22.
+//
+
+import Foundation
+
+class SnailFishNumber {
+    
+    var parent: SnailFishPair? = nil
+    
+    init() {
+    }
+}
