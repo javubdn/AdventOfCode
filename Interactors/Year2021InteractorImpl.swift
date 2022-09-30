eca6f7db50425cc514ff71fe1c8e479f3b5a1a7f
Javi <javi_castillo18@hotmail.com>
2022-09-30 09:20:02 +0200

    Create solution for question 1 in day 20 for year 2021

--- Interactors/Year2021InteractorImpl.swift
+++ Interactors/Year2021InteractorImpl.swift
@@ -1487,6 +1487,14 @@ private extension Year2021InteractorImpl {
     
     @objc
     func day20question1() -> String {
+        let input = readCSV("InputYear2021Day20").components(separatedBy: "\n\n")
+        let algorithm = input[0]
+        var image = input[1].components(separatedBy: .newlines).map { Array($0).map { String($0) } }
+        image = getSharpImage(image, algorithm, 2)
+        let result = image.map { $0.filter { $0 == "#" }.count }.reduce(0, +)
+        return "\(result)"
+    }
+    
         
         for _ in 0..<2 {
             let borderValue = borderBlack ? "." : "#"
