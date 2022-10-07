3b5bab40c5e87a778a181a2e0529ff216580b31b
Javi <javi_castillo18@hotmail.com>
2022-10-07 08:55:22 +0200

    Create convenience init with a string input for RebootStep class

--- Utils/Models/RebootStep.swift
+++ Utils/Models/RebootStep.swift
@@ -27,4 +27,20 @@ class RebootStep {
         self.maxZ = maxZ
     }
     
+    convenience init(_ input: String) {
+        let elements = input.components(separatedBy: ",")
+        let firstPart = elements[0].components(separatedBy: "=")
+        let intervalX = firstPart[1].components(separatedBy: "..")
+        let on = firstPart[0].components(separatedBy: .whitespaces)[0] == "on"
+        let minX = Int(intervalX[0])!
+        let maxX = Int(intervalX[1])!
+        let intervalY = elements[1].components(separatedBy: "=")[1].components(separatedBy: "..")
+        let minY = Int(intervalY[0])!
+        let maxY = Int(intervalY[1])!
+        let intervalZ = elements[2].components(separatedBy: "=")[1].components(separatedBy: "..")
+        let minZ = Int(intervalZ[0])!
+        let maxZ = Int(intervalZ[1])!
+        self.init(on, minX, maxX, minY, maxY, minZ, maxZ)
+    }
+    
 }
