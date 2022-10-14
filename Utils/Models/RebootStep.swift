1f7ac288d768dc7126efb50a455b32dcc2b587e0
Javi <javi_castillo18@hotmail.com>
2022-10-14 08:57:39 +0200

    Create method to solve the cubes question in RebootStep

--- Utils/Models/RebootStep.swift
+++ Utils/Models/RebootStep.swift
@@ -64,6 +64,13 @@ class RebootStep {
         (maxX - minX + 1) * (maxY - minY + 1) * (maxZ - minZ + 1) * (on ? 1 : -1)
     }
     
+    static func solve(_ cubesToUse: [RebootStep]) -> Int {
+        var volumes: [RebootStep] = []
+        cubesToUse.forEach { cube in
+            volumes.append(contentsOf: volumes.compactMap { $0.intersect(cube) })
+            if cube.on { volumes.append(cube) }
+        }
+        return volumes.map { $0.volume() }.reduce(0, +)
     }
     
 }
