ca4378eedec45499a8cffb31e4153306965c3cc8
Javi <javi_castillo18@hotmail.com>
2022-11-02 16:52:37 +0100

    get possiblePositions filtering by correctPosition

--- Utils/Models/DiagramSituation.swift
+++ Utils/Models/DiagramSituation.swift
@@ -72,6 +72,7 @@ class Amphipod {
         }
         
         let correctPosition = diagram.amphipodTarget(self, amphipods)
+        let possiblePositions = positions.filter { correctPosition.0 == $0.0 && correctPosition.1 == $0.1 }
         if status == .middle {
             let correctPosition = diagram.amphipodTarget(self, amphipods)
             positions = positions.filter { correctPosition.0 == $0.0 && correctPosition.1 == $0.1 }
