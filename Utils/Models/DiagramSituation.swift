ef93a4cb876f6778ea8c3fb755e7f087f7aa50da
Javi <javi_castillo18@hotmail.com>
2022-11-03 08:55:37 +0100

    Create allSituations dictionary variable

--- Utils/Models/DiagramSituation.swift
+++ Utils/Models/DiagramSituation.swift
@@ -150,6 +150,7 @@ class DiagramSituation {
     }
     
     func calculateMovements() -> Int {
+        var allSituations: [AmphipodsSituation: Int] = [:]
         var situations = [(AmphipodsSituation(amphipods: amphipods), 0)]
         while !situations.isEmpty {
             let situation = situations.removeFirst()
