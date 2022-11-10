ad5f4dfe235f1c7accd06a4bc55c27c80a3470e6
Javi <javi_castillo18@hotmail.com>
2022-11-10 11:50:26 +0100

    Add case for bronze in forbiddenPositions switch

--- Utils/Models/DiagramSituation.swift
+++ Utils/Models/DiagramSituation.swift
@@ -211,6 +211,7 @@ class DiagramSituation {
         var positions = [(1, 3), (1, 5), (1, 7), (1, 9)]
         switch type {
         case .amber: positions.append(contentsOf: [(2, 5), (3, 5), (4, 5), (5, 5), (2, 7), (3, 7), (4, 7), (5, 7), (2, 9), (3, 9), (4, 9), (5, 9)])
+        case .bronze: positions.append(contentsOf: [(2, 3), (3, 3), (4, 3), (5, 3), (2, 7), (3, 7), (4, 7), (5, 7), (2, 9), (3, 9), (4, 9), (5, 9)])
         }
         return positions
     }
