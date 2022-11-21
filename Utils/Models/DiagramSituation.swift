16bd868547fe452e1a8fb0ccfc46b8094d123962
Javi <javi_castillo18@hotmail.com>
2022-11-21 12:23:57 +0100

    Add filter in else validation

--- Utils/Models/DiagramSituation.swift
+++ Utils/Models/DiagramSituation.swift
@@ -78,6 +78,8 @@ class Amphipod {
         } else if correctPosition.1 {
             positions = possiblePositions.count == 0 ? positions : possiblePositions
         } else {
+            positions = positions.filter { position in
+                !diagram.targets[type]!.contains { position.0 == $0.0 && position.1 == $0.1 }
             }
         }
         return positions
