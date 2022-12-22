2342908aa7a56fe8d43b991f2f63f6c86686bfc9
Javi <javi_castillo18@hotmail.com>
2022-12-22 08:51:13 +0100

    Comment execution in day 23 exercise 1

--- Interactors/Year2021InteractorImpl.swift
+++ Interactors/Year2021InteractorImpl.swift
@@ -1659,6 +1659,11 @@ private extension Year2021InteractorImpl {
 ////        ])
 //        let neighbours = [[2], [1, 3], [2, 4, 6], [3, 5], [4], [3, 7], [6, 8, 10], [7, 9], [8], [7, 11], [10, 12, 14], [11, 13], [12], [11, 15], [14, 16, 18], [15, 17], [16], [15, 19], [18]
 //        ]
+//        let burrowSystem = BurrowSystem(burrow: burrow, neighbours: neighbours)
+//        let result = burrowSystem.solve()
+//        return "\(result)"
+    }
+    
 //                             Room(type: .amber, id: 1, occupant: Amphipod(type: .desert, position: (0, 0))),
 //                             Room(type: .amber, id: 2, occupant: Amphipod(type: .desert, position: (0, 0))),
 //                             Hallway(id: 4), Hallway(id: 5),
