46c88fd4b8f8ab132060249d55f6db0647cbf398
Javi <javi_castillo18@hotmail.com>
2023-01-02 12:06:39 +0100

    Create Year2022InteractorImpl class

---
+++ Interactors/Year2022InteractorImpl.swift
@@ -0,0 +1,13 @@
+//
+//  Year2022InteractorImpl.swift
+//  AdventOfCode
+//
+//  Created by Javier Castillo on 22/12/22.
+//
+
+import Foundation
+
+class Year2022InteractorImpl: NSObject {
+    
+}
+
