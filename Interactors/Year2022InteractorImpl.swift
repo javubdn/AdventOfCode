f550637ffd2f4a56893f12075d9a2ec0a3e84827
Javi <javi_castillo18@hotmail.com>
2023-01-03 09:51:52 +0100

    Create solution for question 1 in day 1 for year 2022

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -13,5 +13,10 @@ class Year2022InteractorImpl: NSObject {
 
 extension Year2022InteractorImpl: YearInteractor {
     
+    @objc
+    func day1question1() -> String {
+        "\(readCSV("InputYear2022Day1").components(separatedBy: "\n\n").map { $0.components(separatedBy: .newlines).map { Int($0)! }.reduce(0, +) }.max()!)"
+    }
+    
     
 }
