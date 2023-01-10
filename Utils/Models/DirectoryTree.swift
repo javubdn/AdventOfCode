e50b4a0de48324a93a672e89c26beef8608790e6
Javi <javi_castillo18@hotmail.com>
2023-01-10 09:02:02 +0100

    Create DirectoryTree protocol

---
+++ Utils/Models/DirectoryTree.swift
@@ -0,0 +1,13 @@
+//
+//  DirectoryTree.swift
+//  AdventOfCode
+//
+//  Created by Javier Castillo on 29/12/22.
+//
+
+protocol DirectoryTree {
+    var size: Int { get }
+    var parent: Directory? {get set}
+    func setParent(_ parent: Directory?)
+}
+
