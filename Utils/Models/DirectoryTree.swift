312961ea07aa37eee218fa8667386f7296e68fb1
Javi <javi_castillo18@hotmail.com>
2023-01-11 10:22:30 +0100

    Create name and parent variables in Directory class

--- Utils/Models/DirectoryTree.swift
+++ Utils/Models/DirectoryTree.swift
@@ -12,10 +12,18 @@ protocol DirectoryTree {
 }
 
 class Directory: DirectoryTree {
+    let name: String
     var size: Int {
         children.map { $0.size }.reduce(0, +)
     }
     var children: [DirectoryTree] = []
+    var parent: Directory?
+    
+    init(_ name: String, parent: Directory? = nil) {
+        self.name = name
+        self.parent = parent
+    }
+    
 }
 
 }
