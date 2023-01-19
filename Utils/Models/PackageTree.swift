b45097b5667f2f7b7664bfdd3356cb0e446faafe
Javi <javi_castillo18@hotmail.com>
2023-01-19 08:51:40 +0100

    Create convenience init for PackageTree class

--- Utils/Models/PackageTree.swift
+++ Utils/Models/PackageTree.swift
@@ -17,4 +17,44 @@ class PackageTree {
         self.packages = packages
     }
     
+    convenience init(_ input: String) {
+        var item = input
+        var values: [PackageTree] = []
+        var currentValue = 0
+        var thereisValue = false
+        while !item.isEmpty {
+            let currentLetter = item.removeFirst()
+            switch currentLetter {
+            case "[":
+                let package = PackageTree(value: nil, packages: [])
+                values.append(package)
+            case "]":
+                if thereisValue {
+                    let package = PackageTree(value: currentValue, packages: nil)
+                    if values.last!.packages == nil { values.last!.packages = [] }
+                    values.last!.packages!.append(package)
+                    currentValue = 0
+                    thereisValue = false
+                }
+                guard values.count > 1 else {
+                    break
+                }
+                let lastPackage = values.removeLast()
+//                if values.last!.packages == nil { values.last!.packages = [] }
+                values.last!.packages!.append(lastPackage)
+            case ",":
+                if thereisValue {
+                    let package = PackageTree(value: currentValue, packages: nil)
+//                    if values.last!.packages == nil { values.last!.packages = [] }
+                    values.last!.packages!.append(package)
+                    currentValue = 0
+                    thereisValue = false
+                }
+            default: currentValue = currentValue * 10 + Int(String(currentLetter))!
+                thereisValue = true
+            }
+        }
+        self.init(value: nil, packages: values.first!.packages)
+    }
+    
 }
