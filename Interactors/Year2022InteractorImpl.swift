275143431bd035da27289b316f3666e2f0d474f9
Javi <javi_castillo18@hotmail.com>
2023-01-20 09:18:11 +0100

    Remove example from question 1 in day 13 for year 2022

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -534,31 +534,6 @@ extension Year2022InteractorImpl: YearInteractor {
     
     @objc
     func day13question1() -> String {
-//        let input = """
-//[1,1,3,1,1]
-//[1,1,5,1,1]
-//
-//[[1],[2,3,4]]
-//[[1],4]
-//
-//[9]
-//[[8,7,6]]
-//
-//[[4,4],4,4]
-//[[4,4],4,4,4]
-//
-//[7,7,7,7]
-//[7,7,7]
-//
-//[]
-//[3]
-//
-//[[[]]]
-//[[]]
-//
-//[1,[2,[3,[4,[5,6,7]]]],8,9]
-//[1,[2,[3,[4,[5,6,0]]]],8,9]
-//"""
         let input = readCSV("InputYear2022Day13")
         let pairs = input.components(separatedBy: "\n\n").map { $0.components(separatedBy: .newlines) }
         let result = pairs.enumerated()
