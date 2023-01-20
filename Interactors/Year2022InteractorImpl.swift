e6c816ee8dd00e6ec944e43618b86ddbd60079e0
Javi <javi_castillo18@hotmail.com>
2023-01-20 09:18:18 +0100

    Remove example from question 2 in day 13 for year 2022

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -545,31 +545,6 @@ extension Year2022InteractorImpl: YearInteractor {
     
     @objc
     func day13question2() -> String {
-//        var input = """
-//[1,1,3,1,1]
-//[1,1,5,1,1]
-//
-//[[1],[2,3,4]]
-//[[1],4]
-//
-//[9]
-//[[8,7,6]]
-//
-//[[4,4],4,4]
-//[[4,4],4,4,4]
-//
-//[7,7,7,7]
-//[7,7,7]
-//
-//[]
-//[3]
-//
-//[[[]]]
-//[[]]
-//
-//[1,[2,[3,[4,[5,6,7]]]],8,9]
-//[1,[2,[3,[4,[5,6,0]]]],8,9]
-//"""
         var input = readCSV("InputYear2022Day13")
         input = input.replacingOccurrences(of: "\n\n", with: "\n")
         input.append("\n[[2]]\n[[6]]")
