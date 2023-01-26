35fdda50b43727b7be5cf59cb1ad1d218b563604
Javi <javi_castillo18@hotmail.com>
2023-01-26 09:16:17 +0100

    Create loop to paint rocks in printS method

--- Utils/Models/SandMap.swift
+++ Utils/Models/SandMap.swift
@@ -113,6 +113,10 @@ class SandMap {
     
     func printS() {
         var sandMap: [[String]] = [[String]](repeating: [String](repeating: ".", count: maxX-minX+3), count: maxY+1)
+        for rc in rock {
+            sandMap[rc.y][rc.x-minX+1] = "#"
+        }
+        
     }
     
 }
