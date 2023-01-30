74bcedfcdb36e45ce517939fa848417161b0f9d9
Javi <javi_castillo18@hotmail.com>
2023-01-30 10:42:04 +0100

    Create solution for part2 in sandMap class

--- Utils/Models/SandMap.swift
+++ Utils/Models/SandMap.swift
@@ -172,6 +172,13 @@ class SandMap {
         dropSand(maxY + 1)
     }
     
+    func solutionPart2() -> Int {
+        let minX: Int = rock.min { $0.x < $1.x }!.x
+        let maxX: Int = rock.max { $0.x > $1.x }!.x
+        for newX in minX-maxY...maxX+maxY {
+            rock.insert(Position(x: newX, y: maxY + 2))
+        }
+        return dropSand(maxY + 3) + 1
     }
     
 }
