047b8db0ad0e00266acd18e6bf7e9879938649d1
Javi <javi_castillo18@hotmail.com>
2023-02-02 09:38:40 +0100

    Create line method in Position class

--- Utils/Models/Position.swift
+++ Utils/Models/Position.swift
@@ -21,4 +21,22 @@ class Position: Hashable {
         lhs.x == rhs.x && lhs.y == rhs.y
     }
     
+    func line(_ to: Position, _ rangeX: (Int, Int)? = nil, _ rangeY: (Int, Int)? = nil ) -> [Position] {
+        guard let rangeX = rangeX, let rangeY = rangeY else {
+            return lineNoRange(to)
+        }
+        if inRange(rangeX, rangeY) && to.inRange(rangeX, rangeY) {
+            return lineNoRange(to)
+        }
+        if inRange(rangeX, rangeY) && !to.inRange(rangeX, rangeY) {
+            let middlePoint = lineOnRect(self, to, rangeX.0, rangeY.0, rangeX.1, rangeY.1)
+            return lineNoRange(Position(x: middlePoint.0, y: middlePoint.1))
+        }
+        if inRange(rangeX, rangeY) && !to.inRange(rangeX, rangeY) {
+            let middlePoint = lineOnRect(self, to, rangeX.0, rangeY.0, rangeX.1, rangeY.1)
+            return Position(x: middlePoint.0, y: middlePoint.1).lineNoRange(to)
+        }
+        return []
+    }
+    
 }
