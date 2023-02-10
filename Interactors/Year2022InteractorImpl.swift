4944cc87a0f370599b871d32eea0f6f6e80e6ea9
Javi <javi_castillo18@hotmail.com>
2023-02-10 11:22:42 +0100

    Remove comment for First loop solution

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -617,23 +617,6 @@ extension Year2022InteractorImpl: YearInteractor {
         let beaconRules = input.components(separatedBy: .newlines).map { getBeaconRule($0) }
         let beacons = Set(beaconRules.map { Position(x: $0.beaconX, y: $0.beaconY) })
         var solution = (0, 0)
-//    extenalLoop:
-//        for x in 0...4_000_000 {
-//            for y in 0...4_000_000 {
-//                var found = true
-//                for beaconRule in beaconRules {
-//                    if abs(x-beaconRule.sensorX)+abs(y-beaconRule.sensorY) <= beaconRule.radius {
-//                        found = false
-//                        break
-//                    }
-//                }
-//                if found {
-//                    solution = (x, y)
-//                    break extenalLoop
-//                }
-//            }
-//        }
-        
         for beaconRule in beaconRules {
             var allPoints: Set<Position> = Set([])
             let up = Position(x: beaconRule.sensorX, y: beaconRule.sensorY - beaconRule.radius - 1)
