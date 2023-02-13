84f0b6b68fd568671ad28e0d76a6bebe429dd20e
Javi <javi_castillo18@hotmail.com>
2023-02-13 16:11:47 +0100

    Remove beaconRules in day 15 of 2022

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -610,6 +610,7 @@ extension Year2022InteractorImpl: YearInteractor {
     @objc
     func day15question2() -> String {
 //        let input = readCSV("InputYear2022Day15")
+//        let beaconRules = input.components(separatedBy: .newlines).map { getBeaconRule($0) }
 //            allPoints.formUnion(up.line(right, (0, 4_000_000), (0, 4_000_000)))
 //            allPoints.formUnion(down.line(right, (0, 4_000_000), (0, 4_000_000)))
 //            allPoints.formUnion(left.line(up, (0, 4_000_000), (0, 4_000_000)))
