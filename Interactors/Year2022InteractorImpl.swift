ef9fc094cd0b6791485a032dc6bbfbae059a62cd
Javi <javi_castillo18@hotmail.com>
2023-02-14 10:12:41 +0100

    Remove pointsUpRight variable in day 15 for 2022

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -619,6 +619,7 @@ extension Year2022InteractorImpl: YearInteractor {
 //            let left = Position(x: beaconRule.sensorX - beaconRule.radius - 1, y: beaconRule.sensorY)
 //            let right = Position(x: beaconRule.sensorX + beaconRule.radius + 1, y: beaconRule.sensorY)
 //
+//            let pointsUpRight = up.line(right, (0, 4_000_000), (0, 4_000_000))
 //            allPoints.formUnion(up.line(right, (0, 4_000_000), (0, 4_000_000)))
 //            allPoints.formUnion(down.line(right, (0, 4_000_000), (0, 4_000_000)))
 //            allPoints.formUnion(left.line(up, (0, 4_000_000), (0, 4_000_000)))
