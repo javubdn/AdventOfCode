c6aa33b8170f629dc34d6007e0f720fd3373cd30
Javi <javi_castillo18@hotmail.com>
2023-02-14 10:12:52 +0100

    Remove pointsDownRight variable in day 15 for 2022

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -620,6 +620,7 @@ extension Year2022InteractorImpl: YearInteractor {
 //            let right = Position(x: beaconRule.sensorX + beaconRule.radius + 1, y: beaconRule.sensorY)
 //
 //            let pointsUpRight = up.line(right, (0, 4_000_000), (0, 4_000_000))
+//            let pointsDownRight = down.line(right, (0, 4_000_000), (0, 4_000_000))
 //            allPoints.formUnion(up.line(right, (0, 4_000_000), (0, 4_000_000)))
 //            allPoints.formUnion(down.line(right, (0, 4_000_000), (0, 4_000_000)))
 //            allPoints.formUnion(left.line(up, (0, 4_000_000), (0, 4_000_000)))
