9ac5e836f89379fd2545296e8dfd1f7a8693cb6c
Javi <javi_castillo18@hotmail.com>
2023-02-27 11:56:27 +0100

    Comment EE valve

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -666,7 +666,6 @@ extension Year2022InteractorImpl: YearInteractor {
         return BeaconRule(sensorX: sensorX, sensorY: sensorY, beaconX: beaconX, beaconY: beaconY)
     }
     
-Valve EE has flow rate=3; tunnels lead to valves FF, DD
 Valve FF has flow rate=0; tunnels lead to valves EE, GG
 Valve GG has flow rate=0; tunnels lead to valves FF, HH
 Valve HH has flow rate=22; tunnel leads to valve GG
