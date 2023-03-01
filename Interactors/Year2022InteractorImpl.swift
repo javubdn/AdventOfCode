2d06a2c9b558d0336320e7259e1f411fa37c4a7f
Javi <javi_castillo18@hotmail.com>
2023-03-01 11:55:22 +0100

    Comment BB valve

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -666,7 +666,6 @@ extension Year2022InteractorImpl: YearInteractor {
         return BeaconRule(sensorX: sensorX, sensorY: sensorY, beaconX: beaconX, beaconY: beaconY)
     }
     
-//Valve BB has flow rate=13; tunnels lead to valves CC, AA
 //Valve CC has flow rate=2; tunnels lead to valves DD, BB
 //Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
 //Valve EE has flow rate=3; tunnels lead to valves FF, DD
