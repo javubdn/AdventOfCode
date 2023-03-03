27778587a297ddb25a95c367c47895db6a293c2e
Javi <javi_castillo18@hotmail.com>
2023-03-03 10:29:35 +0100

    Create valves constant in valves problem

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -724,6 +724,7 @@ Valve JJ has flow rate=21; tunnel leads to valve II
         let match = matches.first!
         let name = String(input[Range(match.range(at: 1), in: input)!])
         let rate = Int(String(input[Range(match.range(at: 2), in: input)!]))!
+        let valves = String(input[Range(match.range(at: 6), in: input)!]).components(separatedBy: ", ")
     }
     
 }
