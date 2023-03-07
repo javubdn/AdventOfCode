c818b32493c47c2543fcfab313e40e1ed11cbfda
Javi <javi_castillo18@hotmail.com>
2023-03-07 14:09:10 +0100

    Create newRemaining constant

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -743,4 +743,5 @@ Valve JJ has flow rate=21; tunnel leads to valve II
         let missing = valves.filter { $0.rate > 0 && !opened.contains($0.name) }
         var bestCost = Int.min
             let time = navigateValve(current, miss.name, valves, [])
+            let newRemaining = remaining - time - 1
 }
