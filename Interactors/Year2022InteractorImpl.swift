4978d2b6c12d028b3100232c19b29775ee31193a
Javi <javi_castillo18@hotmail.com>
2023-03-09 09:11:00 +0100

    Remove opened variable

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -668,7 +668,6 @@ extension Year2022InteractorImpl: YearInteractor {
     
         while !states.isEmpty {
             if !currentState.1.contains(currentState.0) && !valvesNotOpened.contains(currentState.0) {
-                var opened = currentState.1
                 opened.append(currentState.0)
                 if opened.count == maximumOpened {
                     let remainingMinutes = currentState.3 - 1
