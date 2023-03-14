1f5613e20ef7e7e1ded3875184e516252bc7bb6d
Javi <javi_castillo18@hotmail.com>
2023-03-14 12:31:49 +0100

    Create bestSolution constant for question 1 in day 16 for 2022

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -680,6 +680,7 @@ extension Year2022InteractorImpl: YearInteractor {
 //"""
         let input = readCSV("InputYear2022Day16")
         let valvesPath = ValvesPathFinder(input)
+        let bestSolution = valvesPath.searchPaths("AA", 30)
     struct Valve {
         let name: String
         let rate: Int
