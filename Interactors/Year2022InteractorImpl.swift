e228b174633782ee86a903dfe1f2bff4f1bbf62f
Javi <javi_castillo18@hotmail.com>
2023-03-28 09:02:40 +0200

    Remove II valve from the example

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -667,7 +667,6 @@ extension Year2022InteractorImpl: YearInteractor {
     }
     
 //        let input = """
-//Valve II has flow rate=0; tunnels lead to valves AA, JJ
 //Valve JJ has flow rate=21; tunnel leads to valve II
 //"""
         let input = readCSV("InputYear2022Day16")
