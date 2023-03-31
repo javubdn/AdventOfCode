8b37f2e30dfc7870c9039b713306954930b6b197
Javi <javi_castillo18@hotmail.com>
2023-03-31 10:03:17 +0200

    Remove append in opened variable miss name

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -678,7 +678,6 @@ extension Year2022InteractorImpl: YearInteractor {
     func recursiveStuff(_ valves: [Valve], _ remaining: Int, _ opened: [String], _ current: String) -> Int {
         for miss in missing {
             if newRemaining > 0 {
-                opened.append(miss.name)
                 newCost += recursiveStuff(valves, newRemaining, opened, miss.name)
                 bestCost = max(bestCost, newCost)
             } else {
