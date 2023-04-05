6548fbfc80f1376776a59758e7a77c9032254dba
Javi <javi_castillo18@hotmail.com>
2023-04-05 10:10:53 +0200

    Remove opened var declaration

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -681,7 +681,6 @@ extension Year2022InteractorImpl: YearInteractor {
     
     private func newStuff(_ valves: [Valve], _ initial: String, _ time: Int) -> Int {
         for permutation in permutations {
-            var opened: [String] = []
             for item in permutation {
                 let timeCost = navigateValve(current, item.name, valves, [])
                 if timeRemaining > timeCost + 1 {
