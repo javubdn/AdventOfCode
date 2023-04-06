070b11b29fd44204b3ab39d5ffd87cc0e34633a4
Javi <javi_castillo18@hotmail.com>
2023-04-06 09:49:34 +0200

    Remove bestCost return

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -680,7 +680,6 @@ extension Year2022InteractorImpl: YearInteractor {
     }
     
     private func newStuff(_ valves: [Valve], _ initial: String, _ time: Int) -> Int {
-        return bestCost
     }
     
     private func lastStuff(_ valves: [Valve], _ initial: String, _ time: Int) -> Int {
