eda89c635455a26bcce572bead21f0462e1a32c9
Javi <javi_castillo18@hotmail.com>
2023-04-11 09:46:58 +0200

    Remove openedValves append value

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -684,7 +684,6 @@ extension Year2022InteractorImpl: YearInteractor {
     private func lastStuff(_ valves: [Valve], _ initial: String, _ time: Int) -> Int {
         /*
         while remainingTime > 0 {
-            openedValves.append(bestValve.0.name)
             currentValve = bestValve.0.name
             closedValves.removeAll { $0.name == bestValve.0.name }
             if closedValves.isEmpty {
