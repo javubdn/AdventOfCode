dcb775d2fda16e90cfbc19a5e8797a999ea7ec3a
Javi <javi_castillo18@hotmail.com>
2023-04-11 09:47:46 +0200

    Remove condition for closedValves is empty

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -684,9 +684,6 @@ extension Year2022InteractorImpl: YearInteractor {
     private func lastStuff(_ valves: [Valve], _ initial: String, _ time: Int) -> Int {
         /*
         while remainingTime > 0 {
-            if closedValves.isEmpty {
-                break
-            }
         }
         bestResult += remainingTime * pressureReleased(valves, openedValves)
         return bestResult
