bed7325c7091ffd8c5c56a977bd9bf589f21e895
Javi <javi_castillo18@hotmail.com>
2023-04-12 09:32:26 +0200

    Remove zValues variable declaration

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -683,7 +683,6 @@ extension Year2022InteractorImpl: YearInteractor {
     
     private func lastStuff(_ valves: [Valve], _ initial: String, _ time: Int) -> Int {
         /*
-        var zValues = [0: (0, 0)]
         
         var zValuesThisRound: [Int: (Int, Int)] = [:]
         
