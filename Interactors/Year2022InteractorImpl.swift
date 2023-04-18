23e0421bf2d164c79fa5b99e757d20596d0e2a26
Javi <javi_castillo18@hotmail.com>
2023-04-18 09:08:37 +0200

    Remove loop for valveName variable

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -688,8 +688,6 @@ extension Year2022InteractorImpl: YearInteractor {
     }
     
     func navigateValve(_ from: String, _ to: String, _ valves: [Valve], _ used: [String]) -> Int {
-        for valveName in fromValve.valves {
-        }
         return bestCost == Int.max ? Int.max : bestCost + 1
     }
     
