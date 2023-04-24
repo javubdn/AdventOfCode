38c6c8967b3e92a7a117b248b21a5d5c65bcaeae
Javi <javi_castillo18@hotmail.com>
2023-04-24 12:23:36 +0200

    Create fall method in Tetris class

--- Utils/Models/Tetris.swift
+++ Utils/Models/Tetris.swift
@@ -42,9 +42,12 @@ class Tetris {
         self.init(directions)
     }
     
+    private func fall(_ piece: [(Int, Int)]) -> ([(Int, Int)], Bool) {
         guard piece.first(where: { $0.0 == tetrisMap.count - 1 }) == nil else { return (piece, true) }
         guard piece.first(where: { tetrisMap[$0.0+1][$0.1] }) == nil else { return (piece, true) }
         let newPiece = piece.map { ($0.0 + 1, $0.1) }
         return (newPiece, false)
+    }
+    
     
 }
