18029f798c0afa946fe37508f477e3291c54017c
Javi <javi_castillo18@hotmail.com>
2023-04-24 12:25:15 +0200

    Create move method in Tetris class

--- Utils/Models/Tetris.swift
+++ Utils/Models/Tetris.swift
@@ -49,9 +49,12 @@ class Tetris {
         return (newPiece, false)
     }
     
+    private func move(_ piece: [(Int, Int)], _ left: Bool) -> [(Int, Int)] {
         let valid = left ? (piece.min { $0.1 < $1.1 }!.1 > 0) : (piece.max { $0.1 < $1.1 }!.1 < 6)
         guard valid else { return piece }
         guard piece.first(where: { tetrisMap[$0.0][$0.1 + (left ? -1 : 1)] }) == nil else { return piece }
         return piece.map { ($0.0, $0.1 + (left ? -1 : 1)) }
+    }
+    
     
 }
