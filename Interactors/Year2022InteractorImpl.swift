052d5e21948f6891b4d8b17bb59c6298f7ba389e
Javi <javi_castillo18@hotmail.com>
2023-05-05 09:24:35 +0200

    Calculate result for tetris in question 2 in day 17 for 2022

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -692,6 +692,7 @@ extension Year2022InteractorImpl: YearInteractor {
     
         let input = readCSV("InputYear2022Day17")
         let tetris = Tetris(input)
+        let result = tetris.startFall(1_000_000_000_000)
     }
     
     }
