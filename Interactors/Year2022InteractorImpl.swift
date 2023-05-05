3db95941b86a75aa51947ebf1d4091e88d7c71c6
Javi <javi_castillo18@hotmail.com>
2023-05-05 09:24:46 +0200

    Return result in question 2 in day 17 for 2022

--- Interactors/Year2022InteractorImpl.swift
+++ Interactors/Year2022InteractorImpl.swift
@@ -693,6 +693,7 @@ extension Year2022InteractorImpl: YearInteractor {
         let input = readCSV("InputYear2022Day17")
         let tetris = Tetris(input)
         let result = tetris.startFall(1_000_000_000_000)
+        return "\(result)"
     }
     
     }
