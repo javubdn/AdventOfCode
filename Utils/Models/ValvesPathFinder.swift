47a46f3c7ad084934d10da45a6b39eecc4484b6c
Javi <javi_castillo18@hotmail.com>
2023-06-07 15:56:42 +0200

    Crete zeroFlowRooms constant with valves filter

--- Utils/Models/ValvesPathFinder.swift
+++ Utils/Models/ValvesPathFinder.swift
@@ -60,6 +60,7 @@ class ValvesPathFinder {
             }
         }
         
+        let zeroFlowRooms = valves.filter { $0.rate == 0 || $0.name == "AA" }.map { $0.name }
         s.keys.forEach { key in
             zeroFlowRooms.forEach { s[key]![$0] = nil }
         }
