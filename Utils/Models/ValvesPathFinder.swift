a92312f696070c30a28bbfaa804d07bcbb33ba83
Javi <javi_castillo18@hotmail.com>
2023-06-26 10:27:20 +0200

    Create guard to validate combinationCosts count

--- Utils/Models/ValvesPathFinder.swift
+++ Utils/Models/ValvesPathFinder.swift
@@ -172,6 +172,7 @@ class ValvesPathFinder {
                                totalFlow + flow
             )
         }
+        guard combinationCosts.count > 0 else { return totalFlow }
     }
     
 }
