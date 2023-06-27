f8a52961d754f36ce99fc84113a84b5801b4078e
Javi <javi_castillo18@hotmail.com>
2023-06-27 11:43:40 +0200

    Create getValidValves method in ValvesPathFinder class

--- Utils/Models/ValvesPathFinder.swift
+++ Utils/Models/ValvesPathFinder.swift
@@ -183,7 +183,10 @@ class ValvesPathFinder {
         return combinationCosts.max()!
     }
     
+    private func getValidValves(_ location: String, _ seen: Set<String>, _ timeTaken: Int, _ timeAllowed: Int) -> [String: Int] {
         return cheapestPathCosts[location]!
             .filter { !seen.contains($0.key) }
             .filter { $0.value + timeTaken + 1 < timeAllowed }
+    }
+    
 }
