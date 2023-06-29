8e49ca77d8b016fea9c89e615378c1c0843aba49
Javi <javi_castillo18@hotmail.com>
2023-06-29 09:53:50 +0200

    Remove first item from states to assign to state constant

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -29,4 +29,5 @@ class RobotFactory {
         
             var maxGeodes = 0
             var states = [StateRobot(a: 1, b: 0, c: 0, d: 0, e: 1, f: 0, g: 0, h: 0, i: 1)]
+                let state = states.removeFirst()
 }
