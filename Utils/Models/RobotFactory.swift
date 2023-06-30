a23f1cc95d58bbcc8aa7dc72197ed2ec6555b118
Javi <javi_castillo18@hotmail.com>
2023-06-30 11:39:34 +0200

    Calculate result by recurs call

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -41,6 +41,7 @@ class RobotFactory {
         }
         
             let stateMain = StateRobot(a: 1, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: cycles)
+            let result = recurs(stateMain)
 //            }
 //            return bestResult
 }
