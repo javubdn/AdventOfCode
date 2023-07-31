eb8ecd7885ac26c925b65da6240f63b8048c8700
Javi <javi_castillo18@hotmail.com>
2023-07-31 11:27:15 +0200

    Update a value

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -250,4 +250,5 @@ class RobotFactory {
         
             var (a, b, c, d, e, f, g, h, i) = (state.a, state.b, state.c, state.d, state.e, state.f, state.g, state.h, state.i)
                 e -= oreCost
+                a += 1
 }
