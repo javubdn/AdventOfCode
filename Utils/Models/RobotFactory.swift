c1a5777076086b58c7774355c3e53d121571efcc
Javi <javi_castillo18@hotmail.com>
2023-08-03 10:35:30 +0200

    Create matches constant by regex application

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -293,4 +293,5 @@ class RobotFactory {
     var robotMap: [RobotMap] = []
     
         let regex = try! NSRegularExpression(pattern: #"Blueprint ([0-9]+): Each ore robot costs ([0-9]+) ore. Each clay robot costs ([0-9]+) ore. Each obsidian robot costs ([0-9]+) ore and ([0-9]+) clay. Each geode robot costs ([0-9]+) ore and ([0-9]+) obsidian."#)
+        let matches = regex.matches(in: input, options: [], range: NSRange(input.startIndex..., in: input))
 }
