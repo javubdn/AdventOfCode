5683c28da109f6845ece4cb5f88c19e880d4b225
Javi <javi_castillo18@hotmail.com>
2023-08-04 12:28:19 +0200

    Create constant geodeObsidianCost to get the geodeObsidianCost from the match

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -301,4 +301,5 @@ class RobotFactory {
         let obsidianOreCost = Int(String(input[Range(match.range(at: 4), in: input)!]))!
         let obsidianClayCost = Int(String(input[Range(match.range(at: 5), in: input)!]))!
         let geodeOreCost = Int(String(input[Range(match.range(at: 6), in: input)!]))!
+        let geodeObsidianCost = Int(String(input[Range(match.range(at: 7), in: input)!]))!
 }
