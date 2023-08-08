27f5e0accbba6014061b3390b46cd05f7c7d73ff
Javi <javi_castillo18@hotmail.com>
2023-08-08 10:03:02 +0200

    Get geodeOreCost from match value

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -16,6 +16,7 @@ class RobotFactory {
         let clayOreCost = Int(String(input[Range(match.range(at: 3), in: input)!]))!
         let obsidianOreCost = Int(String(input[Range(match.range(at: 4), in: input)!]))!
         let obsidianClayCost = Int(String(input[Range(match.range(at: 5), in: input)!]))!
+        let geodeOreCost = Int(String(input[Range(match.range(at: 6), in: input)!]))!
     class RobotMap {
         let id: Int
         let oreCost: Int
