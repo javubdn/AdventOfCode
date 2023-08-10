21fe1e440725f31620577f48caf3b525a9703ed0
Javi <javi_castillo18@hotmail.com>
2023-08-10 11:02:49 +0200

    Create states variable with a Heap of StateRobots

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -34,6 +34,7 @@ class RobotFactory {
     }
     
         var maxGeodes = 0
+        var states = Heap(elements: [StateRobot()]) { $0.geodes > $1.geodes }
     class RobotMap {
         let id: Int
         let oreCost: Int
