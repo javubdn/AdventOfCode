1393b80290e8d73a3010243ff9fa843b33909ea9
Javi <javi_castillo18@hotmail.com>
2023-08-14 11:09:52 +0200

    Add parameter clayRobots in init method for StateRobot

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -59,6 +59,7 @@ class RobotFactory {
              _ ore: Int = 1,
              _ oreRobots: Int = 1,
              _ clay: Int = 0,
+             _ clayRobots: Int = 0,
     class RobotMap {
         let id: Int
         let oreCost: Int
