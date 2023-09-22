89db5b8b1b4633eca6be3b7d0185f6a93863bbd2
Javi <javi_castillo18@hotmail.com>
2023-09-22 10:06:48 +0200

    Remove ores variable in getStates method

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -199,7 +199,6 @@ class RobotFactory {
     /*
     class RobotMap {
         private func getStates(_ state: (Int, Int, Int, Int, Int, Int, Int, Int, Int)) -> [(Int, Int, Int, Int, Int, Int, Int, Int, Int)] {
-            var ores = state.4
             var clays = state.5
             var obsids = state.6
             var geos = state.7
