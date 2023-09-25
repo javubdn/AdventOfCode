ece6f2f2963db18f6fc117e1a0fc464568016147
Javi <javi_castillo18@hotmail.com>
2023-09-25 11:17:37 +0200

    Create var newClays in getStates method

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -199,7 +199,6 @@ class RobotFactory {
     /*
     class RobotMap {
         private func getStates(_ state: (Int, Int, Int, Int, Int, Int, Int, Int, Int)) -> [(Int, Int, Int, Int, Int, Int, Int, Int, Int)] {
-            var newClays = false
             var newObs = false
             var newGeos = false
             
