4a9cdb13807c9cc64b9f1003b1ce3073f4097971
Javi <javi_castillo18@hotmail.com>
2023-09-26 09:19:31 +0200

    Remove ores update in obsidianCost condition

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -200,7 +200,6 @@ class RobotFactory {
     class RobotMap {
         private func getStates(_ state: (Int, Int, Int, Int, Int, Int, Int, Int, Int)) -> [(Int, Int, Int, Int, Int, Int, Int, Int, Int)] {
 //            if obsidianCost.0 <= ores && obsidianCost.1 <= clays {
-//                ores -= obsidianCost.0
 //                clays -= obsidianCost.1
 //            }
 //            if clayCost <= ores {
