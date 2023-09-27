cfb95e0cdac7dacee7cbfc33fec1df28e32988bb
Javi <javi_castillo18@hotmail.com>
2023-09-27 10:21:46 +0200

    Remove clays update with clayBots sum

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -199,7 +199,6 @@ class RobotFactory {
     /*
     class RobotMap {
         private func getStates(_ state: (Int, Int, Int, Int, Int, Int, Int, Int, Int)) -> [(Int, Int, Int, Int, Int, Int, Int, Int, Int)] {
-//            clays += clayBots
 //            obsids += obsBots
 //            geos += geoBots
 //            oreBots += newOres ? 1 : 0
