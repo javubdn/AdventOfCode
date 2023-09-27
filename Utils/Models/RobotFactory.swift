5fa56b8e1954e226ba57e47774eb4ef94d339f94
Javi <javi_castillo18@hotmail.com>
2023-09-27 10:21:10 +0200

    Remove oreCost condition

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -199,8 +199,6 @@ class RobotFactory {
     /*
     class RobotMap {
         private func getStates(_ state: (Int, Int, Int, Int, Int, Int, Int, Int, Int)) -> [(Int, Int, Int, Int, Int, Int, Int, Int, Int)] {
-//            if oreCost <= ores {
-//            }
 //            ores += oreBots
 //            clays += clayBots
 //            obsids += obsBots
