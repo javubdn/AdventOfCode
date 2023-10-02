558caf5e2e75de4bb9fd0cde4420795c530a2aee
Javi <javi_castillo18@hotmail.com>
2023-10-02 10:36:49 +0200

    Remove canClays declaration

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -200,7 +200,6 @@ class RobotFactory {
     class RobotMap {
         private func isPossibleState(_ state: (Int, Int, Int, Int), _ combination: (Bool, Bool, Bool, Bool)) -> Bool {
             if combination.1 {
-                let (canClays, oresAfterClay) = canGetClay(state.0)
                 guard canClays else { return false }
                 state.0 = oresAfterClay
             }
