59345e535689019270b8d78e9cbba532cc2c0b1e
Javi <javi_castillo18@hotmail.com>
2023-10-02 10:37:20 +0200

    Remove state zero assignation with oresAfterClay

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -200,7 +200,6 @@ class RobotFactory {
     class RobotMap {
         private func isPossibleState(_ state: (Int, Int, Int, Int), _ combination: (Bool, Bool, Bool, Bool)) -> Bool {
             if combination.1 {
-                state.0 = oresAfterClay
             }
             if combination.2 {
                 let (canObs, (oresAfterObs, claysAfterObs)) = canGetObs(state.0, state.1)
