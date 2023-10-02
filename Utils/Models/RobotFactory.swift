9b2d4d338b62900ab3180de06ed754aa7c9beba2
Javi <javi_castillo18@hotmail.com>
2023-10-02 10:37:35 +0200

    Remove combination one condition

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -199,8 +199,6 @@ class RobotFactory {
     /*
     class RobotMap {
         private func isPossibleState(_ state: (Int, Int, Int, Int), _ combination: (Bool, Bool, Bool, Bool)) -> Bool {
-            if combination.1 {
-            }
             if combination.2 {
                 let (canObs, (oresAfterObs, claysAfterObs)) = canGetObs(state.0, state.1)
                 guard canObs else { return false }
