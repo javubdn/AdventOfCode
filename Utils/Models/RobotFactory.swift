d187a0a10f53f1910afc133e62f9061a8ebf7e36
Javi <javi_castillo18@hotmail.com>
2023-10-06 14:45:40 +0200

    Remove newState 4 assign with state 0

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -200,7 +200,6 @@ class RobotFactory {
     class RobotMap {
         private func getNewState(_ state: (Int, Int, Int, Int, Int, Int, Int, Int, Int),
                                  _ combination: (Bool, Bool, Bool, Bool)) -> (Int, Int, Int, Int, Int, Int, Int, Int, Int) {
-            newState.4 += state.0
             newState.5 += state.1
             newState.6 += state.2
             newState.7 += state.3
