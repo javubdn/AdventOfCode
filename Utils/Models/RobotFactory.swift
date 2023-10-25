58dcd937a8dda6550b208264a799707206a1f8f6
Javi <javi_castillo18@hotmail.com>
2023-10-25 12:15:56 +0200

    Create regex for addMap method

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -198,7 +198,6 @@ class RobotFactory {
 }
     /*
     func addMap(_ input: String) {
-        let regex = try! NSRegularExpression(pattern: #"Blueprint ([0-9]+): Each ore robot costs ([0-9]+) ore. Each clay robot costs ([0-9]+) ore. Each obsidian robot costs ([0-9]+) ore and ([0-9]+) clay. Each geode robot costs ([0-9]+) ore and ([0-9]+) obsidian."#)
         let matches = regex.matches(in: input, options: [], range: NSRange(input.startIndex..., in: input))
         let match = matches.first!
         let id = Int(String(input[Range(match.range(at: 1), in: input)!]))!
