f79b6f799d9791faaa89a6c1d81e8b3f647233b4
Javi <javi_castillo18@hotmail.com>
2023-10-25 12:15:46 +0200

    Add new line

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -197,7 +197,6 @@ class RobotFactory {
     }
 }
     /*
-    
     func addMap(_ input: String) {
         let regex = try! NSRegularExpression(pattern: #"Blueprint ([0-9]+): Each ore robot costs ([0-9]+) ore. Each clay robot costs ([0-9]+) ore. Each obsidian robot costs ([0-9]+) ore and ([0-9]+) clay. Each geode robot costs ([0-9]+) ore and ([0-9]+) obsidian."#)
         let matches = regex.matches(in: input, options: [], range: NSRange(input.startIndex..., in: input))
