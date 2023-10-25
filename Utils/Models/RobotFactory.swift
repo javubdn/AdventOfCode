336fcf6c851b8daa5551d1ed664af331683880f3
Javi <javi_castillo18@hotmail.com>
2023-10-25 12:16:06 +0200

    Create matches from regex

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -198,7 +198,6 @@ class RobotFactory {
 }
     /*
     func addMap(_ input: String) {
-        let matches = regex.matches(in: input, options: [], range: NSRange(input.startIndex..., in: input))
         let match = matches.first!
         let id = Int(String(input[Range(match.range(at: 1), in: input)!]))!
         let oreCost = Int(String(input[Range(match.range(at: 2), in: input)!]))!
