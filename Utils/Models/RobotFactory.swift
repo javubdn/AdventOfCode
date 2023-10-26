6da84a1ed6d0c9a7cf94793b9b237146a4f00542
Javi <javi_castillo18@hotmail.com>
2023-10-26 10:19:24 +0200

    Create oreCost value from match 2

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -198,7 +198,6 @@ class RobotFactory {
 }
     /*
     func addMap(_ input: String) {
-        let oreCost = Int(String(input[Range(match.range(at: 2), in: input)!]))!
         let clayOreCost = Int(String(input[Range(match.range(at: 3), in: input)!]))!
         let obsidianOreCost = Int(String(input[Range(match.range(at: 4), in: input)!]))!
         let obsidianClayCost = Int(String(input[Range(match.range(at: 5), in: input)!]))!
