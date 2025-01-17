aa1ac53fd8d0a0dc9ea6211ee78cccc1b8d64225
Javi <javi_castillo18@hotmail.com>
2025-01-17 09:59:24 +0100

    Create main router

---
+++ AdventOfCode/AdventOfCode/Routers/MainRouter.swift
@@ -0,0 +1,12 @@
+//
+//  MainRouter.swift
+//  AdventOfCode
+//
+//  Created by Javier Castillo on 23/6/22.
+//
+
+import Foundation
+
+protocol MainRouter {
+    func openDetail(_ year: Year, _ day: Int)
+}
