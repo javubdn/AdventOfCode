b2bb93b86eef30ff7ef172ae9e727e36c5f438e8
Javi <javi_castillo18@hotmail.com>
2025-01-17 09:58:51 +0100

    Create year selector presenter

---
+++ AdventOfCode/AdventOfCode/Presenters/YearSelectorPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  YearSelectorPresenter.swift
+//  AdventOfCode
+//
+//  Created by Javier Castillo on 9/12/21.
+//
+
+import Foundation
+
+protocol YearSelectorPresenter {
+    
+    func navigateTo(year: Year)
+    
+}
