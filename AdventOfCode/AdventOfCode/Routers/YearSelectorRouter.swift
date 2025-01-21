9ac1764e5d473a30e16479ee00b6662e4330dbf0
Javi <javi_castillo18@hotmail.com>
2025-01-21 09:16:33 +0100

    Create year selector router

---
+++ AdventOfCode/AdventOfCode/Routers/YearSelectorRouter.swift
@@ -0,0 +1,14 @@
+//
+//  YearSelectorRouter.swift
+//  AdventOfCode
+//
+//  Created by Javier Castillo on 9/12/21.
+//
+
+import Foundation
+
+protocol YearSelectorRouter {
+    
+    func navigateTo(year: Year)
+    
+}
