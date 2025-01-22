f4ec6f8fe96305c9e5937ad65d1cbe8f785f1c86
Javi <javi_castillo18@hotmail.com>
2025-01-22 11:48:52 +0100

    Create LavaDrop class

---
+++ AdventOfCode/AdventOfCode/Utils/Models/LavaDrop.swift
@@ -0,0 +1,44 @@
+//
+//  LavaDrop.swift
+//  AdventOfCode
+//
+//  Created by Javier Castillo on 24/3/23.
+//
+
+import Foundation
+
+class LavaDrop {
+    
+    let x: Int
+    let y: Int
+    let z: Int
+    
+    init(_ x: Int, _ y: Int, _ z: Int) {
+        self.x = x
+        self.y = y
+        self.z = z
+    }
+    
+    func collindant(_ other: LavaDrop) -> Bool {
+        ((x == other.x + 1 || x == other.x - 1) && y == other.y && z == other.z)
+        || ((y == other.y + 1 || y == other.y - 1) && x == other.x && z == other.z)
+        || ((z == other.z + 1 || z == other.z - 1) && x == other.x && y == other.y)
+    }
+    
+    func collindants() -> [LavaDrop] {
+        [LavaDrop(x, y, z-1), LavaDrop(x, y, z+1),
+         LavaDrop(x, y-1, z), LavaDrop(x, y+1, z),
+         LavaDrop(x-1, y, z), LavaDrop(x+1, y, z)]
+    }
+    
+}
+
+extension LavaDrop: Hashable {
+    
+    func hash(into hasher: inout Hasher) { }
+    
+    static func == (lhs: LavaDrop, rhs: LavaDrop) -> Bool {
+        lhs.x == rhs.x && lhs.y == rhs.y && lhs.z == rhs.z
+    }
+    
+}
