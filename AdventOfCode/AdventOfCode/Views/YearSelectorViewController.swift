e8b87d75571f0735c40328eaae743e98851bc0a9
Javi <javi_castillo18@hotmail.com>
2025-01-27 12:39:19 +0100

    Create year selector view controller

---
+++ AdventOfCode/AdventOfCode/Views/YearSelectorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  YearSelectorViewController.swift
+//  AdventOfCode
+//
+//  Created by Javier Castillo on 9/12/21.
+//
+
+import UIKit
+
+class YearSelectorViewController: UIViewController {
+
+    let presenter: YearSelectorPresenter
+    
+    init(presenter: YearSelectorPresenter) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter.navigateTo(year: .twentyThree)
+
+    }
+
+    //MARK: - Actions
+    
+    @IBAction func acceptButtonPressed(_ sender: UIButton) {
+        presenter.navigateTo(year: .twentyThree)
+    }
+    
+}
