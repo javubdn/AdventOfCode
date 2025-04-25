cb0a1b70d7c51fd62f80b606e7905ed96da271e8
Javi Castillo <javi_castillo18@hotmail.com>
2025-04-25 09:41:59 +0200

    Delete line in Pluto

--- Utils/Models/PlutoMaze.swift
+++ Utils/Models/PlutoMaze.swift
@@ -1,6 +1,5 @@
 class PlutoMaze {
     convenience init(from input: [String]) {
-        let hole = PlutoMaze.getHole(input)
         input.enumerated().forEach { y, row in
             row.enumerated().forEach { x, c in
                 let place = PointPlutoMaze(x: x, y: y)
