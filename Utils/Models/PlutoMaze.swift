c328d8e5f3f5588cd979549b4828dbf1f72e2343
Javi Castillo <javi_castillo18@hotmail.com>
2025-05-02 09:05:14 +0200

    Delete line from Pluto

--- Utils/Models/PlutoMaze.swift
+++ Utils/Models/PlutoMaze.swift
@@ -1,6 +1,5 @@
 class PlutoMaze {
     private static func getHole(_ input: [String]) -> ((Int, Int), Int, Int) {
-        input = input.map { $0.replacingOccurrences(of: ".", with: "#") }
         let coordinates = Utils.cartesianProduct(lhs: Array(1...input.count-2), rhs: Array(1...input[2].count-2))
         let firstCorner = coordinates.first { coord in
             guard coord.0 > 0 && coord.1 > 0 && coord.1 < input[coord.0].count - 1 else { return false }
