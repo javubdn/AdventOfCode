4d61a7b1df76fc6ccaf18e1078c0f0707c6edf6a
Javi Castillo <javi_castillo18@hotmail.com>
2025-05-05 11:38:28 +0200

    Delete line

--- Utils/Models/PlutoMaze.swift
+++ Utils/Models/PlutoMaze.swift
@@ -1,6 +1,5 @@
 class PlutoMaze {
     private static func getHole(_ input: [String]) -> ((Int, Int), Int, Int) {
-        let secondCorner = coordinates.first { coord in
         }!
         let thirdCorner = coordinates.first { coord in
             guard coord.0 > 0 && coord.1 > 0 && coord.1 < input[coord.0].count - 1 else { return false }
