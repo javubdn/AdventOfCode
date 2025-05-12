eaecf9a542b47f411dbe7ba97e8a1cee99705023
Javi Castillo <javi_castillo18@hotmail.com>
2025-05-12 10:33:52 +0200

    Remove insert

--- Utils/Models/PlutoMaze.swift
+++ Utils/Models/PlutoMaze.swift
@@ -3,7 +3,6 @@ class PlutoMaze {
     func calculateSteps(recursiveLevels: Bool) -> Int {
         while !movements.isEmpty {
             neighbors.forEach { neighbor in
-                visited.insert(PositionLevel(point: neighbor, level: level))
                 movements.append((neighbor, level, movement.2 + 1))
             }
         }
