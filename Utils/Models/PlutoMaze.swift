f9828d91df1f227db5de9d5ca308043ae474c828
Javi Castillo <javi_castillo18@hotmail.com>
2025-05-12 10:34:00 +0200

    Remove append

--- Utils/Models/PlutoMaze.swift
+++ Utils/Models/PlutoMaze.swift
@@ -3,7 +3,6 @@ class PlutoMaze {
     func calculateSteps(recursiveLevels: Bool) -> Int {
         while !movements.isEmpty {
             neighbors.forEach { neighbor in
-                movements.append((neighbor, level, movement.2 + 1))
             }
         }
         return 0
