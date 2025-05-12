6c2624fd830c89b6908921897a134532fd93f614
Javi Castillo <javi_castillo18@hotmail.com>
2025-05-12 10:33:37 +0200

    Remove neighbors

--- Utils/Models/PlutoMaze.swift
+++ Utils/Models/PlutoMaze.swift
@@ -2,7 +2,6 @@ class PlutoMaze {
     
     func calculateSteps(recursiveLevels: Bool) -> Int {
         while !movements.isEmpty {
-                           || portals.map { $0.point }.contains($0))
                     && !visited.contains(PositionLevel(point: $0, level: level))}
             neighbors.forEach { neighbor in
                 visited.insert(PositionLevel(point: neighbor, level: level))
