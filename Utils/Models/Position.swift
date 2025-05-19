88cdc03799390d1b41960a5f8bb5c21a015619e4
Javi Castillo <javi_castillo18@hotmail.com>
2025-05-19 10:17:36 +0200

    Remove guard in Position class

--- Utils/Models/Position.swift
+++ Utils/Models/Position.swift
@@ -2,7 +2,6 @@
 class Position: Hashable {
     
     func line(_ to: Position, _ rangeX: (Int, Int)? = nil, _ rangeY: (Int, Int)? = nil ) -> [Position] {
-        guard let rangeX = rangeX, let rangeY = rangeY else {
             return lineNoRange(to)
         }
         if inRange(rangeX, rangeY) && to.inRange(rangeX, rangeY) {
