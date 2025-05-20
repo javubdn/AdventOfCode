1b55ba7edf3deb87c8216525ac912c7ff494918e
Javi Castillo <javi_castillo18@hotmail.com>
2025-05-20 09:22:01 +0200

    Delete return

--- Utils/Models/Position.swift
+++ Utils/Models/Position.swift
@@ -2,7 +2,6 @@
 class Position: Hashable {
     func line(_ to: Position, _ rangeX: (Int, Int)? = nil, _ rangeY: (Int, Int)? = nil ) -> [Position] {
         if inRange(rangeX, rangeY) && !to.inRange(rangeX, rangeY) {
-            return Position(x: middlePoint.0, y: middlePoint.1).lineNoRange(to)
         }
         return []
     }
