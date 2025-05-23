5bc4dfef28058b67b5e643107d29605f337f841a
Javi Castillo <javi_castillo18@hotmail.com>
2025-05-23 10:18:10 +0200

    Delete in Position class inRange method header

--- Utils/Models/Position.swift
+++ Utils/Models/Position.swift
@@ -3,7 +3,6 @@ class Position: Hashable {
     
         
     
-    private func inRange(_ rangeX: (Int, Int), _ rangeY: (Int, Int)) -> Bool {
     }
     
     private func lineOnRect(_ insidePoint: Position, _ outsidePoint: Position, _ minX: Int, _ minY: Int, _ maxX: Int, _ maxY: Int) -> (Int, Int) {
