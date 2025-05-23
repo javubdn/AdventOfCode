89d606155e975bddd3cbcdc30006310159a529a4
Javi Castillo <javi_castillo18@hotmail.com>
2025-05-23 10:16:51 +0200

    Delete in Position class while

--- Utils/Models/Position.swift
+++ Utils/Models/Position.swift
@@ -3,7 +3,6 @@ class Position: Hashable {
     
     private func lineNoRange(_ to: Position) -> [Position] {
         
-        while previousPoint.x <= to.x {
         }
         return positions
     }
