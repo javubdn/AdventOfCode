01159274b5441b738a8c803abe214a931c13407e
Javi Castillo <javi_castillo18@hotmail.com>
2025-05-27 09:29:30 +0200

    Remove in class Position the closure

--- Utils/Models/Position.swift
+++ Utils/Models/Position.swift
@@ -3,7 +3,6 @@ class Position: Hashable {
     
         
     
-    }
     
     private func lineOnRect(_ insidePoint: Position, _ outsidePoint: Position, _ minX: Int, _ minY: Int, _ maxX: Int, _ maxY: Int) -> (Int, Int) {
         
