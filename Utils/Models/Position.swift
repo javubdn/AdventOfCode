e32d2e4fdb162fa33c653dfe6faa417fd7ef25ea
Javi Castillo <javi_castillo18@hotmail.com>
2025-05-27 09:29:55 +0200

    Delete Position.swift

--- Utils/Models/Position.swift
+++ Utils/Models/Position.swift
@@ -6,7 +6,6 @@ class Position: Hashable {
     
     private func lineOnRect(_ insidePoint: Position, _ outsidePoint: Position, _ minX: Int, _ minY: Int, _ maxX: Int, _ maxY: Int) -> (Int, Int) {
         
-        let ey = vy > 0 ? maxY : minY
         
         if vx == 0 { return (minX, ey) }
         if vy == 0 { return (ex, minY) }
