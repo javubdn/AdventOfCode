8257dcb5536a7006b516ef8d2ce54bec6a5d385f
Javi Castillo <javi_castillo18@hotmail.com>
2025-05-28 09:53:10 +0200

    Remove lineOnRect method in Position class

--- Utils/Models/Position.swift
+++ Utils/Models/Position.swift
@@ -4,7 +4,6 @@ class Position: Hashable {
         
     
     
-    private func lineOnRect(_ insidePoint: Position, _ outsidePoint: Position, _ minX: Int, _ minY: Int, _ maxX: Int, _ maxY: Int) -> (Int, Int) {
         
         
         
