50829e79db18a494c0d9db621790f0aeea84a896
Javi Castillo <javi_castillo18@hotmail.com>
2025-06-02 12:19:04 +0200

    Remove from init in RebootStep class the variable maxX

--- Utils/Models/RebootStep.swift
+++ Utils/Models/RebootStep.swift
@@ -12,7 +12,6 @@ class RebootStep {
     init(_ on: Bool, _ minX: Int, _ maxX: Int, _ minY: Int, _ maxY: Int, _ minZ: Int, _ maxZ: Int) {
     
     convenience init(_ input: String) {
-        let maxX = Int(intervalX[1])!
         let intervalY = elements[1].components(separatedBy: "=")[1].components(separatedBy: "..")
         let minY = Int(intervalY[0])!
         let maxY = Int(intervalY[1])!
