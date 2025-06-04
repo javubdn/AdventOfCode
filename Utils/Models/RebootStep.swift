0bbd17f04e81a02cb09355bcf735e16ebef8e9d2
Javi Castillo <javi_castillo18@hotmail.com>
2025-06-04 09:57:04 +0200

    Remove line

--- Utils/Models/RebootStep.swift
+++ Utils/Models/RebootStep.swift
@@ -24,7 +24,6 @@ class RebootStep {
     func volume() -> Int {
         (maxX - minX + 1) * (maxY - minY + 1) * (maxZ - minZ + 1) * (on ? 1 : -1)
     }
-    
     static func solve(_ cubesToUse: [RebootStep]) -> Int {
         var volumes: [RebootStep] = []
         cubesToUse.forEach { cube in
