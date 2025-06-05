25d938a3312192c32f6699151c4d6232f8e3ceef
Javi Castillo <javi_castillo18@hotmail.com>
2025-06-05 09:49:52 +0200

    Remove line in RebootStep

--- Utils/Models/RebootStep.swift
+++ Utils/Models/RebootStep.swift
@@ -6,7 +6,6 @@
 //
 import Foundation
 class RebootStep {
-    func intersects(_ other: RebootStep) -> Bool {
     }
     func intersect(_ other: RebootStep) -> RebootStep? {
         guard intersects(other) else { return nil }
