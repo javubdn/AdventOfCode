7e925483d3837fbdcab1b722b9415fd16768f8f9
Javi Castillo <javi_castillo18@hotmail.com>
2025-06-06 09:18:39 +0200

    Delete line in RebootStep class

--- Utils/Models/RebootStep.swift
+++ Utils/Models/RebootStep.swift
@@ -8,7 +8,6 @@ import Foundation
 class RebootStep {
     func intersect(_ other: RebootStep) -> RebootStep? {
         return RebootStep(!on,
-                          max(minY, other.minY),
                           min(maxY, other.maxY),
                           max(minZ, other.minZ),
                           min(maxZ, other.maxZ))
