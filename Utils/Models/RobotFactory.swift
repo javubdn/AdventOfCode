47b393d3c4921e26fe069fede4d08c1a706ff396
Javi Castillo <javi_castillo18@hotmail.com>
2025-07-01 10:11:23 +0200

    Remove line in RobotFactory

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -7,7 +7,6 @@
 import Foundation
 class RobotFactory {
     func calculateGeodesFound(_ blueprint: BluePrint ,_ timeBudget: Int) -> Int {
-        while !states.isEmpty {
         }
         return maxGeodes
     }
