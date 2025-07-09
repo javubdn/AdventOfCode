8fe42cf02069b781cb819efe0327fa629154d1e2
Javi Castillo <javi_castillo18@hotmail.com>
2025-07-09 09:04:18 +0200

    Remove line in RobotFactory

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -10,7 +10,6 @@ class RobotFactory {
         let geodeRobots: Int
         
         init(_ time: Int = 1,
-             _ geodeRobots: Int = 0) {
             self.geodeRobots = geodeRobots
         }
         
