56c666e8ef092bde8f50687474df9b4dcbdd45bc
Javi Castillo <javi_castillo18@hotmail.com>
2025-07-10 08:54:37 +0200

    Remove line in RF

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -8,7 +8,6 @@ import Foundation
 class RobotFactory {
     class StateRobot {
         let geodeRobots: Int
-        }
         
         func canOutproduceBest(_ maxGeodes: Int, _ timeBudget: Int) -> Bool {
             let timeLeft = timeBudget - time
