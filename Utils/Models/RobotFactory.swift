bb12f0dc0463d873a3cdd3a630788b7b8550b520
Javi Castillo <javi_castillo18@hotmail.com>
2025-07-16 09:07:47 +0200

    Remove line

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -9,7 +9,6 @@ class RobotFactory {
     class StateRobot {
         
         func calculateNextStates(_ blueprint: BluePrint, _ timeBudget: Int) -> [StateRobot] {
-            return nextStates.filter { $0.time <= timeBudget }
         }
     }
 
