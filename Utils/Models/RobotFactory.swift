28328843712e1cd1fa5b0b33deabe21b3e1451d7
Javi Castillo <javi_castillo18@hotmail.com>
2025-07-17 09:10:26 +0200

    Delete line in RobotFactory

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -7,7 +7,6 @@
 import Foundation
 class RobotFactory {
     class BluePrint {
-        let id: Int
         let oreRobot: RobotBlueprint
         let clayRobot: RobotBlueprint
         let obsidianRobot: RobotBlueprint
