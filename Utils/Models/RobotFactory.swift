44caae420ebe90649cc4edfeca813f100acf1eb6
Javi Castillo <javi_castillo18@hotmail.com>
2025-07-18 08:31:35 +0200

    Removing

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -9,7 +9,6 @@ class RobotFactory {
     class BluePrint {
         init(_ id: Int,
              _ geodeRobot: RobotBlueprint) {
-            self.clayRobot = clayRobot
             self.obsidianRobot = obsidianRobot
             self.geodeRobot = geodeRobot
         }
