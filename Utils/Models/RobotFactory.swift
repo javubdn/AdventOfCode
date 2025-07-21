d8c6f090ee6f8c226ef59ce12e269e9c322a1eeb
Javi Castillo <javi_castillo18@hotmail.com>
2025-07-21 08:57:14 +0200

    Delete line in RobotFactory class

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -8,7 +8,6 @@ import Foundation
 class RobotFactory {
     class BluePrint {
 
-        func maxClay() -> Int {
             max(oreRobot.clayCost, clayRobot.clayCost, obsidianRobot.clayCost, geodeRobot.clayCost)
         }
         
