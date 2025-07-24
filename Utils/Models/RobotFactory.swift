946840e120a7fe90221ce7c51279d18e8dbd32a7
Javi Castillo <javi_castillo18@hotmail.com>
2025-07-24 08:59:59 +0200

    Remove line

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -10,7 +10,6 @@ class RobotFactory {
     class RobotBlueprint {
         init(_ oreRobotsBuilt: Int,
              _ obsidianCost: Int = 0) {
-            self.oreRobotsBuilt = oreRobotsBuilt
             self.clayRobotsBuilt = clayRobotsBuilt
             self.obsidianRobotsBuilt = obsidianRobotsBuilt
             self.geodeRobotsBuilt = geodeRobotsBuilt
