a9c9c7f95471a5a95f6b7464d94675a8b6d96c26
Javi Castillo <javi_castillo18@hotmail.com>
2025-08-05 11:35:03 +0200

    Remove line

--- Utils/Models/RobotFactory.swift
+++ Utils/Models/RobotFactory.swift
@@ -9,7 +9,6 @@ class RobotFactory {
     class BluePrint {
     class RobotBlueprint {
         private func timeUntilBuild(_ productionState: StateRobot) -> Int {
-            let neededClay = clayCost - productionState.clay
             let neededObsidian = obsidianCost - productionState.obsidian
             if oreRobotsBuilt == 1 || clayRobotsBuilt == 1 {
                 return (neededOre <= 0 ? 0 : Int(ceil(Double(neededOre)/Double(productionState.oreRobots)))) + 1
