6b85b4c4c81f30eeabb58c32308b59d255421609
Javi Castillo <javi_castillo18@hotmail.com>
2025-09-01 09:17:42 +0200

    Remove line

--- Utils/Models/SandMap.swift
+++ Utils/Models/SandMap.swift
@@ -27,7 +27,6 @@ class SandMap {
                         if pile(x+1, y+1) {
                             sand.insert(Position(x: x, y: y))
                             if isFloor {
-                                minX = min(minX, x)
                                 maxX = max(maxX, x)
                             }
                             if y <= 0 {
