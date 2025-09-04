a19d44e91a5782f62873ecfaf06c91aa451edf9e
Javi Castillo <javi_castillo18@hotmail.com>
2025-09-04 11:59:37 +0200

    Remove line

--- Utils/Models/SandMap.swift
+++ Utils/Models/SandMap.swift
@@ -21,7 +21,6 @@ class SandMap {
         while true {
             while y <= maxY {
                 if pile(x, y+1) {
-                    if pile(x-1, y+1) {
                     } else {
                     }
                 }
