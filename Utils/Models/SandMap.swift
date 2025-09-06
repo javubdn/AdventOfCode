575231275b266bc62ccac41793310e8f67c5aeeb
Javi Castillo <javi_castillo18@hotmail.com>
2025-09-06 17:41:41 +0200

    Creating remove for a line

--- Utils/Models/SandMap.swift
+++ Utils/Models/SandMap.swift
@@ -11,7 +11,6 @@ class SandMap {
     private var queue: Set<ActionQueue> = []
     private var isFloor = false
     
-    
     func countSand() -> Int {
         sand.filter { $0.y <= maxY }.count
     }
