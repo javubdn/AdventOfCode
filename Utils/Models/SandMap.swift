5181df218d3aed00da3bc716988d05feb5f18252
Javi Castillo <javi_castillo18@hotmail.com>
2025-09-10 08:41:49 +0200

    Delete line from SandMap

--- Utils/Models/SandMap.swift
+++ Utils/Models/SandMap.swift
@@ -16,7 +16,6 @@ class SandMap {
                 if pile(x-1, y+1) {
                     if pile(x+1, y+1) {
                         sand.insert(Position(x: x, y: y))
-                        if y > 0 {
                         }
                         return
                     } else {
