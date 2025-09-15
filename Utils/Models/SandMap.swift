280e94d755a835833a020a512e794fae32552940
Javi Castillo <javi_castillo18@hotmail.com>
2025-09-15 09:46:17 +0200

    Removing from SandMap

--- Utils/Models/SandMap.swift
+++ Utils/Models/SandMap.swift
@@ -16,7 +16,6 @@ class SandMap {
             let next = [down(start), downLeft(start), downRight(start)].first { !rock.contains($0) }
             if next == nil && start == sandSource {
             } else if next == nil {
-                start = sandSource
             } else if next!.y == voidStartsAt {
                 return landed
             } else {
