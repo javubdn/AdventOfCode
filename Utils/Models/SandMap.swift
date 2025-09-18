297b90bad85a5aaace29faec14bdab769d2a2cfc
Javi Castillo <javi_castillo18@hotmail.com>
2025-09-18 09:11:15 +0200

    Delete the line

--- Utils/Models/SandMap.swift
+++ Utils/Models/SandMap.swift
@@ -17,7 +17,6 @@ class SandMap {
     }
     
     func solutionPart2() -> Int {
-        let minX: Int = rock.min { $0.x < $1.x }!.x
         let maxX: Int = rock.max { $0.x > $1.x }!.x
         for newX in minX-maxY...maxX+maxY {
             rock.insert(Position(x: newX, y: maxY + 2))
