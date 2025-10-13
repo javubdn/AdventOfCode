3c0965a541278447a7dc8b28677e086583ef0202
Javi Castillo <javi_castillo18@hotmail.com>
2025-10-13 11:48:25 +0200

    Remove line

--- Utils/Models/Scanner.swift
+++ Utils/Models/Scanner.swift
@@ -10,7 +10,6 @@ import Foundation
 class Scanner {
     
     convenience init(from input: String) {
-        let matches = regex.matches(in: scannerName, options: [], range: NSRange(scannerName.startIndex..., in: scannerName))
         let match = matches.first!
         let id = Int(String(scannerName[Range(match.range(at: 1), in: scannerName)!]))!
         self.init(id: id, beacons: Set(beacons))
