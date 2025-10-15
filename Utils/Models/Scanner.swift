272e9d4761418f65ba6b2dddea95beeeb152f2ea
Javi Castillo <javi_castillo18@hotmail.com>
2025-10-15 09:04:37 +0200

    Remove line

--- Utils/Models/Scanner.swift
+++ Utils/Models/Scanner.swift
@@ -15,7 +15,6 @@ class Scanner {
     
     func commonBeaconsNoRotation(with other: Scanner) -> ((Int, Int, Int), Set<Beacon>)? {
         for currentBeacon in beacons {
-            for otherBeacon in other.beacons {
             }
         }
         return nil
