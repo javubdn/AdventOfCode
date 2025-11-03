cbc7c22eb19be1c79e85bf10724fa189506a2d7c
Javi Castillo <javi_castillo18@hotmail.com>
2025-11-03 08:29:27 +0100

    Delete line

--- Utils/Models/Shuffler.swift
+++ Utils/Models/Shuffler.swift
@@ -12,7 +12,6 @@ class Shuffler {
     
     
     func modularArithmeticVersion(_ numberCards: Int, _ numberShuffles: Int, _ find: Int) -> Int {
-        let item3 = memory[0].power(numberCards - 2).modulus(numberCardsBI)
         return Int((item1 + item2 * item3).modulus(numberCardsBI))
     }
     
