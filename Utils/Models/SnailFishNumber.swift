6068ff9cf4252424c6d433e76f7282d6fa4947b4
Javi Castillo <javi_castillo18@hotmail.com>
2025-11-12 19:39:30 +0100

    Remove line fromSnailFishNumber

--- Utils/Models/SnailFishNumber.swift
+++ Utils/Models/SnailFishNumber.swift
@@ -10,7 +10,6 @@ import Foundation
 class SnailFishNumber {
     
     func sum(_ other: SnailFishNumber) -> SnailFishPair {
-        self.parent = snail
         other.side = .right
         other.parent = snail
         return snail.optimise()
