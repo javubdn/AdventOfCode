5e3ced6e107595a2c07a2c15897c32116895996d
Javi Castillo <javi_castillo18@hotmail.com>
2025-11-21 09:05:31 +0100

    Remove line

--- Utils/Models/SnailFishNumber.swift
+++ Utils/Models/SnailFishNumber.swift
@@ -10,7 +10,6 @@ import Foundation
 class SnailFishPair: SnailFishNumber {
     
     init(left: SnailFishNumber, right: SnailFishNumber) {
-        self.left.side = .left
         self.right.side = .right
         super.init()
         self.left.parent = self
