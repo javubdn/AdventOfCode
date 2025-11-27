048b205df7e14fd8d3ff49ca60e36cb47dc28c44
Javi Castillo <javi_castillo18@hotmail.com>
2025-11-27 09:06:12 +0100

    Remove line

--- Utils/Models/SnailFishNumber.swift
+++ Utils/Models/SnailFishNumber.swift
@@ -14,7 +14,6 @@ class SnailFishPair: SnailFishNumber {
         for item in input {
             switch item {
             default:
-                let value = Int(String(item))!
                 let snailFishValue = SnailFishValue(value: value)
                 levels[levels.count-1].append(snailFishValue)
             }
