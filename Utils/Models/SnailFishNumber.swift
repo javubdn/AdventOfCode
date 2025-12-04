a476d4eb9330bf7db4d45ebac7e0bf0c2e9effa2
Javi Castillo <javi_castillo18@hotmail.com>
2025-12-04 10:07:39 +0100

    Remove line

--- Utils/Models/SnailFishNumber.swift
+++ Utils/Models/SnailFishNumber.swift
@@ -12,7 +12,6 @@ class SnailFishPair: SnailFishNumber {
     override func findLevel4(_ level: Int) -> SnailFishPair? {
         guard level > 0 else {
             if let right = right.findLevel4(level) {
-                return right
             }
             return self
         }
