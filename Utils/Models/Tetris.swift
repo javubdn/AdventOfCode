3207627c6dddeeb928a29713331de6f788c55747
Javi Castillo <javi_castillo18@hotmail.com>
2026-01-07 09:11:01 +0100

    Remove line

--- Utils/Models/Tetris.swift
+++ Utils/Models/Tetris.swift
@@ -16,7 +16,6 @@ class Tetris {
         
         
         static func == (lhs: Tetris.TetrisState, rhs: Tetris.TetrisState) -> Bool {
-            lhs.peaks == rhs.peaks && lhs.currentBlock == rhs.currentBlock && lhs.currentIndex == rhs.currentIndex
         }
         
     }
