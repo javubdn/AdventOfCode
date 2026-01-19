0aaed2054dd6811428a117acc651ee1010f6e788
Javi Castillo <javi_castillo18@hotmail.com>
2026-01-19 10:34:14 +0100

    Remove line

--- Utils/Models/Tetris.swift
+++ Utils/Models/Tetris.swift
@@ -17,7 +17,6 @@ class Tetris {
     }
     
     private func insertPiece(_ index: Int, _ directionsIndex: Int) -> Int {
-        piece.forEach { tetrisMap[$0.0][$0.1] = true }
         let filesToRemove = tetrisMap.enumerated().first { $0.element.filter { $0 }.count > 0 }!.offset
         tetrisMap.removeFirst(filesToRemove)
         return directionsIndex
