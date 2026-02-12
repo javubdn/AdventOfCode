79081e7a905282ce8ce5d03bdac601b604cd5148
Javi Castillo <javi_castillo18@hotmail.com>
2026-02-12 09:22:53 +0100

    Delete line

--- Utils/Models/Tile.swift
+++ Utils/Models/Tile.swift
@@ -16,7 +16,6 @@ class Tile {
     func combinations() -> [Tile] {
         var rotations = [self]
         for _ in 0..<3 {
-            rotations.append(rotations.last!.rotate())
         }
         let flips = rotations.map { $0.flip() }
         return rotations + flips
