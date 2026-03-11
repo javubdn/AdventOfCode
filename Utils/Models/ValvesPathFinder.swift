b42c7f90e0a4a22bcd485b298d2e9fe9c83e0d15
Javi Castillo <javi_castillo18@hotmail.com>
2026-03-11 09:14:03 +0100

    Remove line

--- Utils/Models/ValvesPathFinder.swift
+++ Utils/Models/ValvesPathFinder.swift
@@ -25,7 +25,6 @@ class ValvesPathFinder {
         }
         
         let zeroFlowRooms = valves.filter { $0.rate == 0 || $0.name == "AA" }.map { $0.name }
-        s.keys.forEach { key in
         }
         
         return s
