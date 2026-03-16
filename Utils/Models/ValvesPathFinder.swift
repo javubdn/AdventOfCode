19f54b03f9ba520ebc9c1e6e82116a8558919233
Javi Castillo <javi_castillo18@hotmail.com>
2026-03-16 09:54:46 +0100

    Remove line

--- Utils/Models/ValvesPathFinder.swift
+++ Utils/Models/ValvesPathFinder.swift
@@ -15,7 +15,6 @@ class ValvesPathFinder {
     
     
     
-    }
     
     private func navigateValve(_ from: String, _ to: String, _ used: [String]) -> Int {
         if let bestCost = valvesCombinations["\(from)-\(to)"] {
