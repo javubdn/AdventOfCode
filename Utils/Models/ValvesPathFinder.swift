1cfb744f966bb70ae1f09a88ca4edd9d8b627e5b
Javi Castillo <javi_castillo18@hotmail.com>
2026-03-16 09:54:43 +0100

    Remove line

--- Utils/Models/ValvesPathFinder.swift
+++ Utils/Models/ValvesPathFinder.swift
@@ -15,7 +15,6 @@ class ValvesPathFinder {
     
     
     
-    private func calculateShortestPaths() -> [String: [String: Int]] {
     }
     
     private func navigateValve(_ from: String, _ to: String, _ used: [String]) -> Int {
