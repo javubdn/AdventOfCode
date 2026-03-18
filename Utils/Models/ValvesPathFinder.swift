74b2a8420a3742737b82887670e988296007dbf0
Javi Castillo <javi_castillo18@hotmail.com>
2026-03-18 09:34:59 +0100

    Remove line

--- Utils/Models/ValvesPathFinder.swift
+++ Utils/Models/ValvesPathFinder.swift
@@ -17,7 +17,6 @@ class ValvesPathFinder {
     
     
     private func navigateValve(_ from: String, _ to: String, _ used: [String]) -> Int {
-        used.append(from)
         var bestCost = Int.max
         let solution = bestCost == Int.max ? Int.max : bestCost + 1
         if solution != Int.max {
