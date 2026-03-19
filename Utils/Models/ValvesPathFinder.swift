9edf0c88bbf0f0f1501bec5d51cac739d3818324
Javi Castillo <javi_castillo18@hotmail.com>
2026-03-19 08:46:09 +0100

    Delete line

--- Utils/Models/ValvesPathFinder.swift
+++ Utils/Models/ValvesPathFinder.swift
@@ -16,7 +16,6 @@ class ValvesPathFinder {
     
     
     
-    private func navigateValve(_ from: String, _ to: String, _ used: [String]) -> Int {
     }
     
     func searchPaths(_ location: String,
