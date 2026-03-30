4d417e3f454cb49259c342dd82052eeb2f7ea0e2
Javi Castillo <javi_castillo18@hotmail.com>
2026-03-30 09:50:06 +0200

    Remove line

--- Utils/Models/ValvesPathFinder.swift
+++ Utils/Models/ValvesPathFinder.swift
@@ -30,7 +30,6 @@ class ValvesPathFinder {
             
             
             let combinationCosts = validCombinations.map { combination in
-                let flow2 = (timeAllowed - timeTaken - cheapestPathCosts[location.1]![combination.1]! - 1) * item2.rate
                 let newTime1 = cheapestPathCosts[location.0]![combination.0]! + 1 - newTimeTaken
                 )
             }
