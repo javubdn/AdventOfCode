60caca2bdafd7a4c4a9c300803723b5b9bf080de
Javi Castillo <javi_castillo18@hotmail.com>
2026-03-31 08:22:32 +0200

    Remove line

--- Utils/Models/ValvesPathFinder.swift
+++ Utils/Models/ValvesPathFinder.swift
@@ -29,7 +29,6 @@ class ValvesPathFinder {
     private func costForDemiMovement(_ locationFinished: String,
                                      _ locationMovement: String,
                                      _ seen: Set<String>,
-                                     _ timeAllowed: Int,
                                      _ timeTaken: Int,
                                      _ timeMovement: Int,
                                      _ totalFlow:Int) -> Int {
