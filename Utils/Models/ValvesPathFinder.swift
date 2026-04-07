31c6357cf3986bf4357084ca7d936538db046d65
Javi Castillo <javi_castillo18@hotmail.com>
2026-04-07 09:09:19 +0200

    Remove line

--- Utils/Models/ValvesPathFinder.swift
+++ Utils/Models/ValvesPathFinder.swift
@@ -25,7 +25,6 @@ class ValvesPathFinder {
                                      _ totalFlow:Int) -> Int {
         let validValves = getValidValves(locationFinished, seen, timeTaken, timeAllowed)
         let combinationCosts = validValves.map { valve in
-            let newTime1 = timeMovement - newTimeTaken
             let newTime2 = valve.value + 1 - newTimeTaken
         }
         guard combinationCosts.count > 0 else { return totalFlow }
