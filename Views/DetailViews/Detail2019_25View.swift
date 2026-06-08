898c6b18676039e79c2fdffb99df5838f08d3459
Javi Castillo <javier.castillo@grifols.com>
2026-06-08 09:39:30 +0200

    Remove line

--- Views/DetailViews/Detail2019_25View.swift
+++ Views/DetailViews/Detail2019_25View.swift
@@ -12,7 +12,6 @@ class Detail2019_25View: UIView {
     
     init(_ mainStackView: UIStackView) {
         
-        answerTextView.delegate = self
         answerTextView.returnKeyType = .done
         
         let actionButton = UIButton()
