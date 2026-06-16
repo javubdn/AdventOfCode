d86811eced4d33a160ee569243f0c7ce123d6aff
Javi Castillo <javier.castillo@grifols.com>
2026-06-16 09:29:46 +0200

    Remove line

--- Views/DetailViews/Detail2019_25View.swift
+++ Views/DetailViews/Detail2019_25View.swift
@@ -63,7 +63,6 @@ extension Detail2019_25View: UITextFieldDelegate {
         }
     }
     
-    private func textViewDidEndEditing(_ textView: UITextView) {
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
