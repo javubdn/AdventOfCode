4ce1f6ad861e6d66d536471c105b59bab242d159
Javi Castillo <javier.castillo@grifols.com>
2026-06-17 09:12:52 +0200

    Remove line

--- Views/DetailViews/Detail2019_25View.swift
+++ Views/DetailViews/Detail2019_25View.swift
@@ -70,7 +70,6 @@ extension Detail2019_25View: UITextFieldDelegate {
         let output = intcode!.readOutput()
         let lines = output.map { value in
         }
-            .trimmingCharacters(in: .newlines)
         instructionsLabel.text = lines
         textField.text = nil
         return true
