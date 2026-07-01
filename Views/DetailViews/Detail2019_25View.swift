5b02df629beb1350e8638df373290bbefa3c022f
Javi Castillo <javier.castillo@grifols.com>
2026-07-01 10:17:24 +0200

    Remove line

--- Views/DetailViews/Detail2019_25View.swift
+++ Views/DetailViews/Detail2019_25View.swift
@@ -7,7 +7,6 @@ import UIKit
 class Detail2019_25View: UIView {
     
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
     }
     func initializeComputer() {
         do {
