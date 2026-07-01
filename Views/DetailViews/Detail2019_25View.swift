ad9ffcc10088f90d4948cef3ab9564568ab3522b
Javi Castillo <javier.castillo@grifols.com>
2026-07-01 10:17:05 +0200

    Remove line

--- Views/DetailViews/Detail2019_25View.swift
+++ Views/DetailViews/Detail2019_25View.swift
@@ -6,7 +6,6 @@ import UIKit
 
 class Detail2019_25View: UIView {
     
-    private let instructionsLabel = UILabel()
     private let answerTextView = UITextField()
     private var intcode: Intcode? = nil
     init(_ mainStackView: UIStackView) {
