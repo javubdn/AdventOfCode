f0b2bd40df021ec3c231bb113da0efe5f7a343e6
Javi Castillo <javier.castillo@grifols.com>
2026-07-01 10:17:15 +0200

    Remove line

--- Views/DetailViews/Detail2019_25View.swift
+++ Views/DetailViews/Detail2019_25View.swift
@@ -6,7 +6,6 @@ import UIKit
 
 class Detail2019_25View: UIView {
     
-    init(_ mainStackView: UIStackView) {
     }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
