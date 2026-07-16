d33eeec43961b08a9fb75081d6b3698455760df4
Javi Castillo <javier.castillo@grifols.com>
2026-07-16 10:05:29 +0200

    Delete line

--- Views/MainViewController.swift
+++ Views/MainViewController.swift
@@ -11,7 +11,6 @@ class MainViewController: UIViewController {
     @IBOutlet weak var yearLabel: UILabel!
     
     
-    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
