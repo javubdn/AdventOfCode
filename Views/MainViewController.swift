fa5dec6a6b07acd04a86529812184ea6185f9ded
Javi Castillo <javier.castillo@grifols.com>
2026-07-16 10:05:23 +0200

    Delete line

--- Views/MainViewController.swift
+++ Views/MainViewController.swift
@@ -12,7 +12,6 @@ class MainViewController: UIViewController {
     
     
     init(presenter: MainPresenter, currentYear: Year) {
-        self.currentYear = currentYear
         super.init(nibName: nil, bundle: nil)
     }
     
