eac72683531d72e93fdc0f126c4211ab92fa1f17
Javi Castillo <javier.castillo@grifols.com>
2026-07-29 09:30:55 +0200

    Delete line

--- Views/YearSelectorViewController.swift
+++ Views/YearSelectorViewController.swift
@@ -12,7 +12,6 @@ class YearSelectorViewController: UIViewController {
     let presenter: YearSelectorPresenter
     
     init(presenter: YearSelectorPresenter) {
-        super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder: NSCoder) {
