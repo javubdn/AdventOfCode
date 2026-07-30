5af76d431647dff8d0dc1704be1815d4f7436d8c
Javi Castillo <javier.castillo@grifols.com>
2026-07-30 08:02:24 +0200

    Delete line

--- Views/YearSelectorViewController.swift
+++ Views/YearSelectorViewController.swift
@@ -11,7 +11,6 @@ class YearSelectorViewController: UIViewController {
 
     let presenter: YearSelectorPresenter
     
-    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
