451fa4e20791197086dc7fc8183d68472c12a44c
Javi Castillo <javier.castillo@grifols.com>
2026-07-30 08:02:26 +0200

    Delete line

--- Views/YearSelectorViewController.swift
+++ Views/YearSelectorViewController.swift
@@ -13,7 +13,6 @@ class YearSelectorViewController: UIViewController {
     
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     override func viewDidLoad() {
