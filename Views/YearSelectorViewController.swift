5f4a854738f5b46f47ddc18d011477440560b4d2
Javi Castillo <javier.castillo@grifols.com>
2026-08-03 09:25:51 +0200

    Remove line

--- Views/YearSelectorViewController.swift
+++ Views/YearSelectorViewController.swift
@@ -13,7 +13,6 @@ class YearSelectorViewController: UIViewController {
     
     
     override func viewDidLoad() {
-        super.viewDidLoad()
         presenter.navigateTo(year: .twentyTwo)
 
     }
