468a34144cbc958a9ce5d31b42881f60a0f1abab
Javi Castillo <javier.castillo@grifols.com>
2026-08-06 09:53:59 +0200

    Remove line

--- Views/DayView.swift
+++ Views/DayView.swift
@@ -13,7 +13,6 @@ protocol DayViewDelegate {
 
 class DayView: UIView {
     
-    private var day: Int
     
     init(_ year: Year, _ day: Int, _ mainStackView: UIStackView, _ delegate: DayViewDelegate) {
         answer1Label = UILabel()
