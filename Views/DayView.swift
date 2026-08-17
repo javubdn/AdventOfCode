06ea36088f17c24bd9f6ddcb91b1f508ba1c8b45
Javi Castillo <javier.castillo@grifols.com>
2026-08-17 09:34:59 +0200

    Remove line

--- Views/DayView.swift
+++ Views/DayView.swift
@@ -16,7 +16,6 @@ class DayView: UIView {
         layer.cornerRadius = 5
 
         let titleDayLabel = UILabel()
-        titleDayLabel.textAlignment = .center
         titleDayLabel.font = UIFont(name: "Futura-Medium", size: 20.0)
 
         let question1Label = UILabel()
