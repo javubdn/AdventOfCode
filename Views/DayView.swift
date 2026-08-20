aef1aedd95e3944d0381f536385d26a08d1dea11
Javi Castillo <javier.castillo@grifols.com>
2026-08-20 09:52:58 +0200

    Delete line

--- Views/DayView.swift
+++ Views/DayView.swift
@@ -14,7 +14,6 @@ protocol DayViewDelegate {
 class DayView: UIView {
     init(_ year: Year, _ day: Int, _ mainStackView: UIStackView, _ delegate: DayViewDelegate) {
 
-        
 
 
         answer2Label.text = "Calculando..."
