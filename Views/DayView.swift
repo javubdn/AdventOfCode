3e811ac17a0732948724ed7a5fb14a673617e403
Javi Castillo <javier.castillo@grifols.com>
2026-08-21 10:34:05 +0200

    Remove line

--- Views/DayView.swift
+++ Views/DayView.swift
@@ -15,7 +15,6 @@ class DayView: UIView {
     init(_ year: Year, _ day: Int, _ mainStackView: UIStackView, _ delegate: DayViewDelegate) {
 
 
-        vStackView.axis = .vertical
         vStackView.spacing = 15
         vStackView.translatesAutoresizingMaskIntoConstraints = false
 
