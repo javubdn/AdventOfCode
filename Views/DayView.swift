d4396cbf6a6e0767c3d86417bee8eae69ab1d7e0
Javi Castillo <javier.castillo@grifols.com>
2026-08-24 10:55:11 +0200

    Remove line

--- Views/DayView.swift
+++ Views/DayView.swift
@@ -20,7 +20,6 @@ class DayView: UIView {
         addSubview(vStackView)
         
         NSLayoutConstraint.activate([
-            vStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
             vStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
             vStackView.topAnchor.constraint(equalTo: topAnchor, constant: 20),
             vStackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20),
