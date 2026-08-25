0f08a50386b1206df7eaf8036a2014100a2bafe1
Javi Castillo <javier.castillo@grifols.com>
2026-08-25 09:49:03 +0200

    Delete line

--- Views/DayView.swift
+++ Views/DayView.swift
@@ -17,7 +17,6 @@ class DayView: UIView {
 
         addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.clickSection(_:))))
         addSubview(vStackView)
-        
         ])
         
     }
