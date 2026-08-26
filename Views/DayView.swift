0e2eca7480d4ebb424b1748114acc23f4c256ae8
Javi Castillo <javier.castillo@grifols.com>
2026-08-26 09:15:28 +0200

    Remove line

--- Views/DayView.swift
+++ Views/DayView.swift
@@ -19,7 +19,6 @@ class DayView: UIView {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        answer2Label = UILabel()
         delegate = nil
         year = .fifteen
         day = 0
