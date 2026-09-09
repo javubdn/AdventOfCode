00f8bd83b815374e4e242d01959c9a22324ee760
Javi Castillo <javier.castillo@grifols.com>
2026-09-09 10:29:05 +0200

    Remove line

--- Utils/Models/WaterMap.swift
+++ Utils/Models/WaterMap.swift
@@ -29,7 +29,6 @@ class WaterMap {
             let rigth = items[1].components(separatedBy: "=")[1].components(separatedBy: "..")
             if left[0] == "x" {
                 for i in Int(rigth[0])!...Int(rigth[1])! {
-                    clay.insert(Position(x: Int(left[1])!, y: i))
                 }
             } else {
                 for i in Int(rigth[0])!...Int(rigth[1])! {
