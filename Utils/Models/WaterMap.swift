bccda9e126feadbcc5e56d62aea665f82848fad4
Javi Castillo <javier.castillo@grifols.com>
2026-09-17 10:15:45 +0200

    Remove line

--- Utils/Models/WaterMap.swift
+++ Utils/Models/WaterMap.swift
@@ -70,7 +70,6 @@ class WaterMap {
         while pile(x1, y + 1) && !stop(x1 + 1, y) {
             x1 += 1
         }
-        let stop1 = stop(x1 + 1, y)
         if stop0 && stop1 {
             for i in x0..<x1+1 {
                 still.insert(Position(x: i, y: y))
