64b5b4f69c7d059ffc88f5e9f02a8611df07d527
Javi Castillo <javier.castillo@grifols.com>
2026-09-17 10:15:37 +0200

    Remove line

--- Utils/Models/WaterMap.swift
+++ Utils/Models/WaterMap.swift
@@ -78,7 +78,6 @@ class WaterMap {
             }
             queue.insert(ActionQueue(action: .scan, x: x, y: y - 1))
         } else {
-            }
             if !stop1 {
                 queue.insert(ActionQueue(action: .fall, x: x1, y: y))
             }
