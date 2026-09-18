f79d9eacb2adfa4cee7934ef85d08bc6f838a341
Javi Castillo <javier.castillo@grifols.com>
2026-09-18 08:14:37 +0200

    Remove line

--- Utils/Models/WaterMap.swift
+++ Utils/Models/WaterMap.swift
@@ -64,7 +64,6 @@ class WaterMap {
     private func scan(_ x: Int, _ y: Int) {
         var x0 = x
         var x1 = x
-        while pile(x1, y + 1) && !stop(x1 + 1, y) {
         }
         if stop0 && stop1 {
             for i in x0..<x1+1 {
