72eb12aa922fc467f2781821dfa9ce1275406b8d
Javi Castillo <javier.castillo@grifols.com>
2026-09-18 08:14:46 +0200

    Remove line

--- Utils/Models/WaterMap.swift
+++ Utils/Models/WaterMap.swift
@@ -62,7 +62,6 @@ class WaterMap {
     }
     
     private func scan(_ x: Int, _ y: Int) {
-        var x1 = x
         if stop0 && stop1 {
             for i in x0..<x1+1 {
                 still.insert(Position(x: i, y: y))
